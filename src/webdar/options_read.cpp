4c80d7a438ab00cbccf93bb1cb49bec018219513
Denis Corbin <dar.linux@free.fr>
2013-03-24 21:41:01 +0100

    added option_read class

---
+++ src/webdar/options_read.cpp
@@ -0,0 +1,154 @@
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "webdar_tools.hpp"
+
+
+    //
+#include "options_read.hpp"
+
+using namespace std;
+
+options_read::options_read():
+    form("Update"),
+    fs_src("Archive Parameters"),
+    fs_ref("Auxilliary Archive Parameters"),
+    src_crypto_algo("Cipher"),
+    src_crypto_pass("Passphrase", html_form_input::password, "", 30),
+    src_crypto_size("Cipher block size", html_form_input::number, "0", 8),
+    src_execute("Command to execute locally before reading each slice", html_form_input::text, "", 30),
+    src_slice_min_digits("Slice minimum digit", html_form_input::number, "0", 8),
+    info_details("Detailed information", html_form_input::check, "", 1),
+    lax("Laxist check mode", html_form_input::check, "", 1),
+    sequential_read("Sequential read", html_form_input::check, "", 1),
+    src_use_external_catalogue("Use external catalogue", html_form_input::check, "", 1),
+    ref_path("Path", html_form_input::text, "", 30),
+    ref_basename("Archive basename", html_form_input::text, "", 30),
+    ref_crypto_algo("Cipher"),
+    ref_crypto_pass("Passphrase", html_form_input::password, "", 30),
+    ref_crypto_size("Cipher block size", html_form_input::number, "0", 8),
+    ref_execute("Command to execute locally before reading each slice", html_form_input::text, "", 30),
+    ref_slice_min_digits("Slice minimum digit", html_form_input::number, "0", 8)
+{
+    libdar::archive_options_read defaults;
+
+	// set default values
+    src_crypto_algo.set_value(defaults.get_crypto_algo());
+    src_crypto_pass.set_value("");
+    src_crypto_size.set_value(webdar_tools_convert_to_string(defaults.get_crypto_size()));
+    src_execute.set_value(defaults.get_execute());
+    src_slice_min_digits.set_value(webdar_tools_convert_to_string(defaults.get_slice_min_digits()));
+    info_details.set_value_as_bool(defaults.get_info_details());
+    lax.set_value_as_bool(defaults.get_lax());
+    sequential_read.set_value_as_bool(defaults.get_sequential_read());
+    src_use_external_catalogue.set_value_as_bool(defaults.is_external_catalogue_set());
+    ref_path.set_value(defaults.get_ref_path().display());
+    ref_basename.set_value(defaults.get_ref_basename());
+    ref_crypto_algo.set_value(defaults.get_ref_crypto_algo());
+    ref_crypto_pass.set_value("");
+    ref_crypto_size.set_value(webdar_tools_convert_to_string(defaults.get_ref_crypto_size()));
+    ref_execute.set_value(defaults.get_ref_execute());
+    ref_slice_min_digits.set_value(webdar_tools_convert_to_string(defaults.get_ref_slice_min_digits()));
+
+	// constructing the view
+    fs_src.adopt(&src_crypto_algo);
+    fs_src.adopt(&src_crypto_pass);
+    fs_src.adopt(&src_execute);
+    fs_src.adopt(&src_slice_min_digits);
+    fs_src.adopt(&info_details);
+    fs_src.adopt(&lax);
+    fs_src.adopt(&sequential_read);
+    fs_src.adopt(&src_use_external_catalogue);
+    fs_ref.adopt(&ref_path);
+    fs_ref.adopt(&ref_basename);
+    fs_ref.adopt(&ref_crypto_algo);
+    fs_ref.adopt(&ref_crypto_pass);
+    fs_ref.adopt(&ref_crypto_size);
+    fs_ref.adopt(&ref_execute);
+    fs_ref.adopt(&ref_slice_min_digits);
+    form.adopt(&fs_src);
+    form.adopt(&fs_ref);
+    div.adopt(&form);
+
+	// binding the events
+    src_crypto_algo.record_actor_on_event(this, html_form_radio::changed);
+    src_use_external_catalogue.record_actor_on_event(this, html_form_input::changed);
+
+    ref_crypto_algo.record_actor_on_event(this, html_form_radio::changed);
+}
+
+libdar::archive_options_read options_read::get_options() const
+{
+    libdar::archive_options_read ret;
+
+    ret.set_crypto_algo(src_crypto_algo.get_value());
+    if(src_crypto_algo.get_value() != libdar::crypto_none)
+    {
+	ret.set_crypto_pass(libdar::secu_string(src_crypto_pass.get_value().c_str(), src_crypto_pass.get_value().size()));
+	ret.set_crypto_size(webdar_tools_convert_to_int(src_crypto_size.get_value()));
+    }
+    ret.set_execute(src_execute.get_value());
+    ret.set_slice_min_digits(libdar::infinint(webdar_tools_convert_to_int(src_slice_min_digits.get_value())));
+    ret.set_info_details(info_details.get_value_as_bool());
+    ret.set_lax(lax.get_value_as_bool());
+    ret.set_sequential_read(sequential_read.get_value_as_bool());
+    if(src_use_external_catalogue.get_value_as_bool())
+    {
+	ret.set_external_catalogue(ref_path.get_value(), ref_basename.get_value());
+	ret.set_ref_crypto_algo(ref_crypto_algo.get_value());
+	if(ref_crypto_algo.get_value() != libdar::crypto_none)
+	{
+	    ret.set_ref_crypto_pass(libdar::secu_string(ref_crypto_pass.get_value().c_str(), ref_crypto_pass.get_value().size()));
+	    ret.set_ref_crypto_size(webdar_tools_convert_to_int(ref_crypto_size.get_value()));
+	}
+	ret.set_ref_execute(ref_execute.get_value());
+	ret.set_ref_slice_min_digits(libdar::infinint(webdar_tools_convert_to_int(ref_slice_min_digits.get_value())));
+    }
+    else
+	ret.unset_external_catalogue();
+
+    return ret;
+}
+
+void options_read::on_event(const std::string & event_name)
+{
+    if(src_crypto_algo.get_value() == 0)
+    {
+	src_crypto_pass.set_visible(false);
+	src_crypto_size.set_visible(false);
+    }
+    else
+    {
+	src_crypto_pass.set_visible(true);
+	src_crypto_size.set_visible(true);
+    }
+
+    if(ref_crypto_algo.get_value() == 0)
+    {
+	ref_crypto_pass.set_visible(false);
+	ref_crypto_size.set_visible(false);
+    }
+    else
+    {
+	ref_crypto_pass.set_visible(true);
+	ref_crypto_size.set_visible(true);
+    }
+
+    if(src_use_external_catalogue.get_value_as_bool())
+	fs_ref.set_visible(false);
+    else
+	fs_ref.set_visible(true);
+}
+
+string options_read::get_body_part(const chemin & path,
+				   const request & req)
+{
+    return div.get_body_part(path, req);
+}
---
+++ src/webdar/options_read.hpp
@@ -0,0 +1,64 @@
+#ifndef OPTIONS_READ_HPP
+#define OPTIONS_READ_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "body_builder.hpp"
+#include "html_div.hpp"
+#include "html_form.hpp"
+#include "html_form_fieldset.hpp"
+#include "html_form_select.hpp"
+#include "html_form_input.hpp"
+#include "html_crypto_algo.hpp"
+#include "events.hpp"
+
+class options_read : public body_builder, public actor
+{
+public:
+    options_read();
+
+    libdar::archive_options_read get_options() const;
+
+	/// inherited from actor
+    void on_event(const std::string & event_name);
+
+	/// inherited from body_builder
+    std::string get_body_part(const chemin & path,
+			      const request & req);
+
+private:
+	// construction fields
+    html_div div;
+    html_form form;
+    html_form_fieldset fs_src;
+    html_form_fieldset fs_ref;
+	// archive_options_read fields
+    html_crypto_algo src_crypto_algo;
+    html_form_input src_crypto_pass;
+    html_form_input src_crypto_size;
+    html_form_input src_execute;
+    html_form_input src_slice_min_digits;
+    html_form_input info_details;
+    html_form_input lax;
+    html_form_input sequential_read;
+	// html_select src_entrepot;
+    html_form_input src_use_external_catalogue;
+    html_form_input ref_path;
+    html_form_input ref_basename;
+    html_crypto_algo ref_crypto_algo;
+    html_form_input ref_crypto_pass;
+    html_form_input ref_crypto_size;
+    html_form_input ref_execute;
+    html_form_input ref_slice_min_digits;
+	// html_select ref_entrepot;
+};
+
+#endif
