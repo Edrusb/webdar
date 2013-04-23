d58b193a5888f2190b9076b324ab6ed6c8086447
Denis Corbin <dar.linux@free.fr>
2013-04-23 23:12:56 +0200

    creating archive_read class

---
+++ src/webdar/archive_read.cpp
@@ -0,0 +1,122 @@
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+
+
+
+    //
+#include "archive_read.hpp"
+
+using namespace std;
+
+archive_read::archive_read(const string & archive_description):
+    form("Update"),
+    fs(archive_description),
+    archive("Archive Basename",
+	    html_form_input::text,
+	    "",
+	    30),
+    show_read_options("Show archive reading options",
+		      html_form_input::check,
+		      "",
+		      1)
+{
+    ptr = NULL;
+
+	/// web components layout
+    form.adopt(&fs);
+    fs.adopt(&archive);
+    fs.adopt(&show_read_options);
+    fs.adopt(&opt_read);
+    adopt(&form);
+
+	/// events
+    show_read_options.record_actor_on_event(this, html_form_input::changed);
+    on_event(html_form_input::changed); // set the object in a coherent status
+}
+
+archive_read::archive_read(const archive_read & ref):
+    form(""),
+    fs(""),
+    archive("",
+	    html_form_input::text,
+	    "",
+	    30),
+    show_read_options("",
+		      html_form_input::check,
+		      "",
+		      1)
+{
+    throw WEBDAR_BUG;
+}
+
+void archive_read::open_archive(/* webinteraction required here */)
+{
+    if(ptr != NULL)
+	throw WEBDAR_BUG;
+
+    try
+    {
+/*
+
+        A IMPLEMENTER
+
+	    /// split archive.get_value() in path and basename
+	libdar::path chem = ...;
+	string::basename;
+	ptr = new archive(web_interaction,
+			  chem,
+			  basename,
+			  /// extension
+			  opt_read.get_options());
+
+	if(ptr == NULL)
+	    throw ...
+*/
+    }
+    catch(libdar::Egeneric & e)
+    {
+/*
+
+  A IMPLEMENTER
+
+*/
+    }
+
+}
+
+libdar::archive & archive_read::get_archive()
+{
+    if(ptr == NULL)
+	throw WEBDAR_BUG;
+    return *ptr;
+}
+
+void archive_read::close_archive()
+{
+    if(ptr == NULL)
+	throw WEBDAR_BUG;
+    delete ptr;
+    ptr = NULL;
+}
+
+string archive_read::get_body_part(const chemin & path,
+				   const request & req)
+{
+    return get_body_part_from_all_children(path, req);
+}
+
+void archive_read::on_event(const std::string & event_name)
+{
+    if(event_name != html_form_input::changed)
+	throw WEBDAR_BUG;
+
+    opt_read.set_visible(show_read_options.get_value_as_bool());
+}
---
+++ src/webdar/archive_read.hpp
@@ -0,0 +1,50 @@
+#ifndef ARCHIVE_READ_HPP
+#define ARCHIVE_READ_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "body_builder.hpp"
+#include "html_form.hpp"
+#include "html_form_input.hpp"
+#include "html_form_fieldset.hpp"
+#include "options_read.hpp"
+#include "events.hpp"
+
+class archive_read: public body_builder, public actor
+{
+public:
+    archive_read(const std::string & archive_description);
+    archive_read(const archive_read & ref);
+    archive_read & operator = (const archive_read & ref) { throw WEBDAR_BUG; };
+    ~archive_read() { if(ptr != NULL) close_archive(); };
+
+    void open_archive(/* webinteraction required here */);
+    libdar::archive & get_archive();
+    void close_archive();
+
+	/// inherited from body_builder
+    virtual std::string get_body_part(const chemin & path,
+				      const request & req);
+
+	/// inherited from events
+    virtual void on_event(const std::string & event_name);
+
+private:
+    html_form form;
+    html_form_fieldset fs;
+    html_form_input archive;
+    html_form_input show_read_options;
+    options_read opt_read;
+
+    libdar::archive *ptr;
+};
+
+#endif
