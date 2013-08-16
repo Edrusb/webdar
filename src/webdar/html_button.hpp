51e80160b694b00564d3c2f59a1da963299297b6
Denis Corbin <dar.linux@free.fr>
2013-08-16 12:32:23 +0200

    adding html_button::change_label() method

--- src/webdar/html_button.hpp
+++ src/webdar/html_button.hpp
@@ -25,6 +25,7 @@ public:
     void set_url_classid(const std::string & classid) { inside.set_class(classid); };
     void set_url_class(const html_url_class & val);
     void set_url_css(const css & val) { inside.css_inherit_from(val); };
+    void change_label(const std::string & label) { inside.change_label(label); };
 
 	// inherited from body_builder
     virtual std::string get_body_part(const chemin & path,
