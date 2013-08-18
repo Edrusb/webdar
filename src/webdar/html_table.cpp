8f116fbc0c05fd4ed32bcdeed25a703a7ff6c41d
Denis Corbin <dar.linux@free.fr>
2013-08-18 19:28:18 +0200

    implemented new methods on html_table class in order to pass css properties to the cells

--- src/webdar/html_table.cpp
+++ src/webdar/html_table.cpp
@@ -16,6 +16,17 @@ extern "C"
 
 using namespace std;
 
+html_table::html_table(unsigned int width)
+{
+    dim_x = width;
+    border_collapsed = true;
+    cells_title_set = false;
+}
+
+void html_table::css_border_collapsed(bool mode)
+{
+    border_collapsed = string(" border-collapse: ") + (mode ? "collapse;" : "separate;");
+}
 
 string html_table::get_body_part(const chemin & path,
 				 const request & req)
@@ -24,30 +35,57 @@ string html_table::get_body_part(const chemin & path,
     bundle bdl;
     chemin sub_path = path;
     unsigned int line_length = 0;
+    bool first_line = true;
+    string cell_css_style = build_style();
+    string table_css = css_get_raw_string() + border_collapsed;
+
+    if(table_css != "")
+	table_css = "style=\"" + table_css + "\"";
 
     if(sub_path.size() > 0)
 	sub_path.pop_front();
 
-    ret = "<table " + css_get_string() + " " + border + ">\n";
+    ret = cell_css_style;
+    ret += "<table " + table_css  + ">\n";
+
     reset_read_next();
     while(read_next(bdl))
     {
+	    /// the <tr> mark for each new line
 	if(line_length == 0)
 	    ret += "<tr>\n";
-	ret += "<td>\n";
+
+	    /// the <td> ... cell ... </td>
+	ret += "<td";
+	if(cell_css_style != "")
+	{
+	    ret += " class=\"";
+
+	    if(first_line && cells_title_set)
+		ret += get_class_id(true);
+	    else
+		ret += get_class_id(false);
+	    ret += "\"";
+	}
+	ret += ">\n";
 	if(bdl.obj != NULL)
 	    ret += bdl.obj->get_body_part(sub_path, req);
 	else
 	    ret += bdl.static_text;
 	ret += "</td>\n";
 
+	    /// counting cell to known when to close and start a new raw
+
 	++line_length;
 	if(line_length == dim_x)
 	{
-	    ret += "</tr>\n";
+	    ret += "</tr>\n"; // end of raw
 	    line_length = 0;
+	    first_line = false;
 	}
     }
+
+	/// adding empty cells if necessary (well, ... that's a bit useless, but we can't know whether a particular browser will not cry on that)
 
     if(line_length != 0)
     {
@@ -59,6 +97,34 @@ string html_table::get_body_part(const chemin & path,
 	ret += "<tr>\n";
     }
     ret += "</table>\n";
+
+    return ret;
+}
+
+string html_table::get_class_id(bool title) const
+{
+    return get_path().namify() + (title ? "T" : "A");
+}
+
+string html_table::build_style() const
+{
+    string ret = "";
+    string tmp;
+
+	// class for title cells
+    if(cells_title_set)
+    {
+	tmp = cells_title.css_get_raw_string();
+	if(tmp != "")
+	    ret += "td." + get_class_id(true) + " { " + tmp + "}\n";
+    }
+	// class for any
+    tmp = cells.css_get_raw_string();
+    if(tmp != "")
+	ret += "td." + get_class_id(false)+ " { " + tmp + "}\n";
+
+    if(ret != "")
+	ret = "<style type=\"text/css\">\n" + ret + "</style>\n";
 
     return ret;
 }
--- src/webdar/html_table.hpp
+++ src/webdar/html_table.hpp
@@ -19,19 +19,41 @@ extern "C"
 class html_table: public html_level
 {
 public:
-    html_table(unsigned int width) { dim_x = width; border = ""; };
+    html_table(unsigned int width);
     html_table(const html_table & ref) { throw WEBDAR_BUG; };
     const html_table & operator = (const html_table & ref) { throw WEBDAR_BUG; };
     ~html_table() { clear(); };
 
-    void set_border(unsigned int width) { border = "border=\"" + webdar_tools_convert_to_string(width) + "\""; };
+	/// whether border are collapsed or separated (collasped by default)
+    void css_border_collapsed(bool mode);
+
+	/// define a specific css object for the _cells_ first raw
+    void set_css_cells_first_raw(const css & val) { cells_title_set = true; cells_title = val; };
+
+	/// clear apreviously assigned css object to the title raw
+    void set_css_cells_first_raw() { cells_title_set = false; cells_title.css_clear_attributes(); };
+
+	/// assigned to all celles except those of the first raw if set_css_cells_first_raw was set
+    void set_css_cells(const css & val) { cells = val; };
+
+	/// clear apreviously assigned css object to the title raw
+    void set_css_cells() { cells.css_clear_attributes(); };
+
+	/// assign to all cells
 
     virtual std::string get_body_part(const chemin & path,
 				      const request & req);
 
 private:
     unsigned int dim_x;
-    std::string border;
+    std::string border_collapsed;
+    bool cells_title_set;
+    css cells_title;
+    css cells;
+
+    std::string get_class_id(bool title) const;
+    std::string build_style() const;
+
 
 };
 
