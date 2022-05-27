0978d5978c6312deaf76f9be54612ad328c67058
Denis corbin <dar.linux@free.fr>
2022-05-27 20:02:41 +0200

    clarifying comments and indentation in class user_interface

--- src/user_interface.hpp
+++ src/user_interface.hpp
@@ -53,7 +53,7 @@ class user_interface : public responder, public events, public actor
 {
 public:
 	/// available event for that class
-    static const std::string closing;      //< signal end of user session
+    static const std::string closing;      ///< signal end of user session
 
     user_interface();
     user_interface(const user_interface & ref) = delete;
@@ -86,34 +86,34 @@ protected:
 private:
     enum mode_type
     {
-	config,        //< should display parameter selection
-	listing_open,  //< should display the web_user_interface, progressive_report an cancellation button
-	listing,       //< should display listing navigation window
-	running,       //< should display web_user_interface, progressive_report and cancellation button
-	error,         //< should display the error
+	config,        ///< should display parameter selection
+	listing_open,  ///< should display the web_user_interface, progressive_report an cancellation button
+	listing,       ///< should display listing navigation window
+	running,       ///< should display web_user_interface, progressive_report and cancellation button
+	error,         ///< should display the error
     };
 
     std::string sessname;  //< customized name of that session (empty string by default)
-    libthreadar::mutex mut_sessname; //< drives the access to sessname, by set and get session name methods
-    mode_type mode;        //< current mode
-    mode_type return_mode; //< mode in which to return from error status
-    bool close_requested;  //< whether session close has been asked
-    bool mode_changed;     //< whether mode has changed
+    libthreadar::mutex mut_sessname; ///< drives the access to sessname, by set and get session name methods
+    mode_type mode;        ///< current mode
+    mode_type return_mode; ///< mode in which to return from error status
+    bool close_requested;  ///< whether session close has been asked
+    bool mode_changed;     ///< whether mode has changed
 
-    saisie parametrage;            //< page issued in mode == config
-    html_libdar_running in_action; //< page issued when a libdar thread is running (mode == running)
-    html_error in_error;           //< page issued when an exception has been caught (mode == error)
-    html_listing_page in_list;     //< page issued when proceeding to archive listing
+    saisie parametrage;            ///< page issued in mode == config
+    html_libdar_running in_action; ///< page issued when a libdar thread is running (mode == running)
+    html_error in_error;           ///< page issued when an exception has been caught (mode == error)
+    html_listing_page in_list;     ///< page issued when proceeding to archive listing
 
-    bool libdar_running;      //< whether a libdar child thread is running
-    libthreadar::thread *current_thread; //< points to the running thread (either arch_test, arch_merge, ....)
-    archive_test arch_test;   //< holds thread created for testing archives
-    archive_restore arch_rest;//< holds thread created for extracting archives
-    archive_compare arch_diff;//< holds thread created for comparing archives
-    archive_create arch_create; //< holds thread created for archive creation
-    archive_isolate arch_isolate; //< holds thread created for archive isolation
-    archive_merge arch_merge; //< holds thread created for archive merging
-    archive_init_list arch_init_list; //< holds thread created to open an archive for listing
+    bool libdar_running;           ///< whether a libdar child thread is running
+    libthreadar::thread *current_thread; ///< points to the running thread (either arch_test, arch_merge, ....)
+    archive_test arch_test;        ///< holds thread created for testing archives
+    archive_restore arch_rest;     ///< holds thread created for extracting archives
+    archive_compare arch_diff;     ///< holds thread created for comparing archives
+    archive_create arch_create;    ///< holds thread created for archive creation
+    archive_isolate arch_isolate;  ///< holds thread created for archive isolation
+    archive_merge arch_merge;      ///< holds thread created for archive merging
+    archive_init_list arch_init_list; ///< holds thread created to open an archive for listing
 
 	/// available parameters for libdar execution
     const saisie & get_parametrage() const { return parametrage; };
