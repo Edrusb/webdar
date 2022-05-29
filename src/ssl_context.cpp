6228eef7226a2e1e5aa51e0e7de4aaab6477f44e
Denis corbin <dar.linux@free.fr>
2022-05-29 17:53:07 +0200

    adding class ssl_connect

---
+++ src/ssl_context.cpp
@@ -0,0 +1,80 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2022 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    //  C system header files
+extern "C"
+{
+}
+
+    // webdar headers
+
+#include "ssl_context.hpp"
+
+using namespace std;
+
+bool ssl_context::initialized = false;
+
+ssl_context::ssl_context(const string & certificate, const string & privatekey)
+{
+    go_init_openssl();
+
+    const SSL_METHOD *ssl_meth = TLS_server_method();
+    if(ssl_meth == nullptr)
+	throw exception_openssl();
+
+    ctx = SSL_CTX_new(ssl_meth);
+    if(ctx == nullptr)
+	throw exception_openssl();
+
+    try
+    {
+	if(! SSL_CTX_use_certificate_file(ctx, certificate.c_str(), SSL_FILETYPE_PEM))
+	    throw exception_openssl();
+
+	if(! SSL_CTX_use_PrivateKey_file(ctx, privatekey.c_str(), SSL_FILETYPE_PEM))
+	    throw exception_openssl();
+
+	if(! SSL_CTX_check_private_key(ctx))
+	    throw exception_openssl();
+    }
+    catch(...)
+    {
+	SSL_CTX_free(ctx);
+	throw;
+    }
+}
+
+
+void ssl_context::go_init_openssl()
+{
+    if(!initialized)
+    {
+	initialized = true;
+
+	SSL_library_init();
+	SSL_load_error_strings();
+	    // this one is deprecated. Should set the OPENSSL_API_COMPAT macro
+	    // https://www.openssl.org/docs/manmaster/man7/openssl_user_macros.html
+    }
+}
+
---
+++ src/ssl_context.hpp
@@ -0,0 +1,73 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2022 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef SSL_CONTEXT_HPP
+#define SSL_CONTEXT_HPP
+
+    // C system header files
+extern "C"
+{
+#include <openssl/ssl.h>
+}
+
+    // C++ system header files
+#include <string>
+
+    // webdar headers
+#include "exceptions.hpp"
+
+    /// manages an SSL context and initiate openssl library
+
+    /// SSL_CTX are openssl structure holding certificate, privatekey
+    /// connection method and algorithms... they are used to create
+    /// an SSL object that ciphers/deciphers/authenticate SSL exchanges
+    /// on a particular connection (for example a TCP connection)
+
+class ssl_context
+{
+public:
+
+	/// constructor
+    ssl_context(const std::string & certificate, const std::string & privatekey);
+
+	/// forbidding copy constuctor and assignment operator
+    ssl_context(const ssl_context & ref) = delete;
+    ssl_context(ssl_context && ref) noexcept = delete;
+    ssl_context & operator = (const ssl_context & ref) = delete;
+    ssl_context & operator = (ssl_context && ref) noexcept = delete;
+
+	/// destructor
+    ~ssl_context() { SSL_CTX_free(ctx); };
+
+    SSL_CTX & get_context() { return *ctx; };
+
+private:
+
+    SSL_CTX *ctx;
+
+    static bool initialized;
+    static void go_init_openssl();
+
+};
+
+#endif
