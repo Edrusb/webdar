fd0e66abcdb4b14b0134bf91341e945eb82e4223
Denis corbin <dar.linux@free.fr>
2022-05-30 18:31:52 +0200

    adding class css_library

---
+++ src/css_library.cpp
@@ -0,0 +1,93 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2022 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "exceptions.hpp"
+#include "css.hpp"
+
+    //
+#include "css_library.hpp"
+
+using namespace std;
+
+void css_library::add(const string & name, const css & value)
+{
+    map<string, css>::iterator it = content.find(name);
+
+    if(it == content.end())
+	content[name] = value;
+    else
+	throw exception_range(string("label ") + name + string(" already present in this css_library\n"));
+}
+
+bool css_library::get_value(const string & name, css & stored_value) const
+{
+    map<string, css>::const_iterator it = content.find(name);
+
+    if(it == content.end())
+	return false;
+    else
+    {
+	stored_value = it->second;
+	return true;
+    }
+}
+
+void css_library::del(const string & name)
+{
+    map<string, css>::const_iterator it = content.find(name);
+
+    if(it == content.end())
+	throw exception_range(string("label ") + name + string(" absent from this css_library, cannot delete it\n"));
+    else
+	content.erase(it);
+}
+
+string css_library::generate_internal_css() const
+{
+    string ret = "<style>\n";
+
+    map<string, css>::const_iterator it = content.begin();
+
+    while(it != content.end())
+    {
+	ret += string(".") + it->first + string(" {");
+	ret += it->second.css_get_raw_string();
+	ret += string(" }\n");
+
+	++it;
+    }
+
+    ret += "</style>\n";
+
+    return ret;
+}
---
+++ src/css_library.hpp
@@ -0,0 +1,62 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2022 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef CSS_LIBRARY_HPP
+#define CSS_LIBRARY_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include <string>
+
+    // webdar headers
+#include "css.hpp"
+
+class css_library
+{
+public:
+
+    css_library() = default;
+    css_library(const css_library & ref) = default;
+    css_library(css_library && ref) noexcept = default;
+    css_library & operator = (const css_library & ref) = default;
+    css_library & operator = (css_library && ref) noexcept = default;
+    ~css_library() = default;
+
+    void add(const std::string & name, const css & value);
+    bool get_value(const std::string & name, css & stored_value) const;
+    void del(const std::string & name);
+    unsigned int size() const { return content.size(); };
+
+    std::string generate_internal_css() const;
+
+private:
+    std::map<std::string, css> content;
+
+};
+
+#endif
