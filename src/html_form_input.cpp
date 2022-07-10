7f4de0ab9fa022b0b1f52bb3e0d706db922f357e
Denis corbin <dar.linux@free.fr>
2022-07-10 22:24:59 +0200

    adapting html_form_input class to the new body_builder interface

--- src/html_form_input.cpp
+++ src/html_form_input.cpp
@@ -105,7 +105,7 @@ string html_form_input::get_body_part(const chemin & path,
     {
 	if(x_type != "checkbox")
 	    ret += "<label for=\"" + x_id + "\">" + x_label + "</label>\n";
-	ret += "<input " + css_get_string() + " type=\"" + x_type + "\" name=\"" + x_id + "\" id=\"" + x_id + "\" ";
+	ret += "<input " + get_css_classes() + " type=\"" + x_type + "\" name=\"" + x_id + "\" id=\"" + x_id + "\" ";
 	if(x_min != "" && x_max != "") // yes both
 	    ret += "min=\"" + x_min + "\" max=\"" + x_max + "\" ";
 	if(x_init != "")
