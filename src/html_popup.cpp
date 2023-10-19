426d9de930490097ac9d9872cc5d2e5a08a8e795
Denis corbin <dar.linux@free.fr>
2023-10-19 22:02:49 +0200

    adding new class html_popup

---
+++ src/html_popup.cpp
@@ -0,0 +1,124 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+
+
+
+    //
+#include "html_popup.hpp"
+
+using namespace std;
+
+const string html_popup::outerclassname = "html_popup_outer";
+
+html_popup::html_popup(unsigned int width_pct, unsigned int height_pct)
+{
+    if(width_pct > 100 || height_pct > 100)
+	throw exception_range("html_popup constructor arguments must be less or equal to 100 (%)");
+    width = width_pct;
+    height = height_pct;
+    innerclassname = "html_popup_" + to_string(width) + "_" + to_string(height);
+
+    	// outerbox must not be adopted, it lives outside any parent/child relationship
+    outerbox.add_css_class(outerclassname);
+    add_css_class(innerclassname);
+}
+
+
+string html_popup::inherited_get_body_part(const chemin & path,
+					 const request & req)
+{
+    ack_visible();
+    if(get_visible())
+    {
+	string ret = outerbox.get_body_part(path, req);
+	ret+= html_div::inherited_get_body_part(path, req);
+	return ret;
+    }
+    else
+	return "";
+}
+
+void html_popup::new_css_library_available()
+{
+    unique_ptr<css_library> & csslib = lookup_css_library();
+
+    if(!csslib)
+	throw WEBDAR_BUG;
+
+    if(!csslib->class_exists(innerclassname))
+    {
+	css innerdef;
+
+	innerdef.css_position_type(css::pos_fixed);
+	innerdef.css_position_top(to_string((100-height)/2)+"%");
+	innerdef.css_position_left(to_string((100-width)/2)+"%");
+	innerdef.css_width(to_string(width)+"%", true);
+	innerdef.css_height(to_string(height)+"%", true);
+	innerdef.css_corner_radius("10px");
+	innerdef.css_padding("5%");
+	innerdef.css_box_shadow();
+	innerdef.css_box_sizing(css::bx_border);
+
+	innerdef.css_z_index(2);
+
+	innerdef.css_border_width(css::bd_all, css::bd_thin);
+	innerdef.css_border_style(css::bd_all, css::bd_double);
+	innerdef.css_border_color(css::bd_all, "rgb(0,0,0)");
+	innerdef.css_background_color("#FFFFFF");
+
+	innerdef.css_opacity("1");
+	csslib->add(innerclassname, innerdef);
+    }
+
+    if(!csslib->class_exists(outerclassname))
+    {
+	css outerdef;
+
+	    // have the outerbox span the whole page
+	outerdef.css_position_type(css::pos_fixed);
+	outerdef.css_position_top("0");
+	outerdef.css_position_bottom("0");
+	outerdef.css_position_left("0");
+	outerdef.css_position_right("0");
+
+	    // visible transparent color (should be parametrable in the future)
+	outerdef.css_opacity("0.5");
+	outerdef.css_background_color("rgb(0, 90, 200)");
+
+	    // and on top of the existing page
+	outerdef.css_z_index(1);
+
+	csslib->add(outerclassname,outerdef);
+    }
+}
---
+++ src/html_popup.hpp
@@ -0,0 +1,82 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_POPUP_HPP
+#define HTML_POPUP_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "body_builder.hpp"
+#include "html_div.hpp"
+
+    /// class html_popup is the implementation of <div> </div> as popup over rest of the the screen
+    ///
+    /// the object shows a full screen with partial opacity (outerbox) on top of which a fully opaque box (innerbox)
+    /// sits. Object adopted by the html_popup are passed to this inner box and take place inside it
+    /// There is no mechanism (button) to remove this box from the screen. This is duty of the class using
+    /// or inheriting from that class to provide such mechanism (making this object invisible() or
+    /// foresaken from the body_building tree for example).
+
+class html_popup : public html_div
+{
+public:
+    html_popup(unsigned int width_pct, unsigned int height_pct);
+    html_popup(const html_popup & ref) = delete;
+    html_popup(html_popup && ref) noexcept = delete;
+    html_popup & operator = (const html_popup & ref) = default;
+    html_popup & operator = (html_popup && ref) noexcept = default;
+    ~html_popup() = default;
+
+protected:
+	/// inherited from body_builder
+    virtual std::string inherited_get_body_part(const chemin & path,
+						const request & req) override;
+
+	/// inherited from body_builder
+    virtual void new_css_library_available() override;
+
+private:
+    static const std::string outerclassname; ///< class name is global to the class
+    std::string innerclassname;              ///< class name depends on the width and height given at construction time
+
+    unsigned int width;         ///< width in percent of the inner div (innerbox)
+    unsigned int height;        ///< height in percent of the inner div (innerbox)
+    bool self_css;             ///< true during initial setup of css class (do not react on css_class_have_chaged()), false otherwise
+
+	/// the outerbox  hides (or partially transparent) and interecpt interaction from the user to
+	/// UI behind the popup. The innerbox shows on top of the outerbox and contains all the components
+	/// the html_popup has adopted.
+	/// the innerbox is us (= this)
+    html_div outerbox;
+
+};
+
+#endif
