b9997f6bcf2f719132d2b3afe79d6779d9b296f5
Denis corbin <dar.linux@free.fr>
2023-10-21 17:49:12 +0200

    new class implementing a popup to read file information on filesystem

---
+++ src/html_select_file.cpp
@@ -0,0 +1,339 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include <utility>
+
+    // webdar headers
+#include "webdar_css_style.hpp"
+
+    //
+#include "html_select_file.hpp"
+
+using namespace std;
+
+    // public static fields:
+const string html_select_file::entry_selected = "entry_selected";
+const string html_select_file::op_cancelled = "op_cancelled";
+
+    // private static fields:
+const string html_select_file::op_chdir_parent = "parent";
+const string html_select_file::op_createdir = "createdir";
+const string html_select_file::css_float_button_right = "html_select_file_btn_right";
+const string html_select_file::css_float_button_left = "html_select_file_btn_left";
+const string html_select_file::css_warning = "html_select_warning";
+const string html_select_file::css_sticky_top = "html_select_sticky_top";
+const string html_select_file::css_sticky_bot = "html_select_sticky_bot";
+
+html_select_file::html_select_file(const std::string & message):
+    html_popup(width_pct,height_pct),
+    status(st_init),
+    only_dir(false),
+    title(2, message),
+    warning(3, ""),
+    fieldset(""),
+    parentdir("Parent Directory", op_chdir_parent),
+    content(2),
+    btn_cancel("Cancel", op_cancelled),
+    btn_validate("Select", entry_selected),
+    btn_createdir("New Folder", op_createdir),
+    ignore_events(false)
+{
+    entr.reset();       // entr points to nothing
+    set_visible(false); // make us invisible until run() is called
+    ack_visible();      // and invisible immediately (acknowlegment)
+
+	// events for callers objects
+    register_name(entry_selected);  // we'll propagate the even from btn_validate
+    register_name(op_cancelled);    // we will propagate the even from btn_cancel
+
+	// events we want to act on from our own html_button
+    btn_cancel.record_actor_on_event(this, op_cancelled);
+    btn_validate.record_actor_on_event(this, entry_selected);
+    parentdir.record_actor_on_event(this, op_chdir_parent);
+    btn_createdir.record_actor_on_event(this, op_createdir);
+
+	// setting up the adoption tree (the fixed part)
+
+    adopt(&title_box);
+    title_box.adopt(&title);
+    title_box.adopt(&warning);
+    adopt(&fieldset);
+    fieldset.adopt(&parentdir);
+    fieldset.adopt(&content);
+    adopt(&btn_box);
+    btn_box.adopt(&btn_createdir);
+    btn_box.adopt(&btn_validate);
+    btn_box.adopt(&btn_cancel);
+
+    title_box.add_css_class(css_sticky_top);
+    warning.add_css_class(css_warning);
+    webdar_css_style::normal_button(btn_cancel);
+    webdar_css_style::normal_button(btn_validate);
+    webdar_css_style::normal_button(btn_createdir);
+    btn_box.add_css_class(css_sticky_bot);
+    btn_cancel.add_css_class(css_float_button_right);
+    btn_validate.add_css_class(css_float_button_right);
+    btn_createdir.add_css_class(css_float_button_left);
+
+}
+
+void html_select_file::run(const shared_ptr<libdar::entrepot> & x_entr,
+			   const std::string & start_dir)
+{
+    switch(status)
+    {
+    case st_init:
+	break;
+    case st_run:
+	return; // already running
+    case st_completed:
+	throw WEBDAR_BUG; // previous value has not been red
+    default:
+	throw WEBDAR_BUG;
+    }
+
+    if(libdar::path(start_dir).is_relative())
+	throw WEBDAR_BUG;
+
+    set_visible(true);
+    ack_visible();
+    status = st_run;
+    entr = x_entr;
+    fieldset.change_label(start_dir);
+    fill_content();
+};
+
+void html_select_file::on_event(const std::string & event_name)
+{
+    if(ignore_events)
+	return;
+
+    if(event_name == entry_selected)
+    {
+	status = st_completed;
+	set_visible(false);
+	ack_visible();
+	act(entry_selected); // propagate the event to object that subscribed to us
+	entr.reset();
+    }
+    else if(event_name == op_cancelled)
+    {
+	status = st_init;
+	set_visible(false);
+	ack_visible();
+	act(op_cancelled);
+	entr.reset();
+    }
+    else if(event_name == op_chdir_parent)
+    {
+	libdar::path chem(fieldset.get_label());
+	string tmp;
+
+	if(!chem.pop(tmp))
+	    warning.add_text(3, string("Cannot go change to parent directory of ") + fieldset.get_label());
+
+	fieldset.change_label(chem.display());
+	fill_content();
+    }
+    else if(event_name == op_createdir)
+    {
+	    // to be implemented
+	throw exception_feature("create directory in entrepot");
+    }
+    else
+    {
+	map<string, html_button*>::iterator it = listed.find(event_name);
+
+	if(it == listed.end())
+	    throw WEBDAR_BUG; // all events we registered for should be known by us
+	if(it->second == nullptr)
+	    throw WEBDAR_BUG;
+
+	    // we concatenate (as a path subdir) the current path with the filename the user has clicked on:
+	fieldset.change_label(libdar::path(fieldset.get_label())
+			      .append((it->second)->get_label())
+			      .display());
+    }
+
+}
+
+string html_select_file::inherited_get_body_part(const chemin & path,
+						 const request & req)
+{
+    if(get_visible())
+    {
+	string ret;
+
+	html_popup::inherited_get_body_part(path, req); // a first time to have the even triggering parameter changes
+	if(entr) // when user finally selects the path, the button event destroys the entrepot, fill_content() cannot work anymore and is not needed neither
+	    fill_content();                             // now we can setup the possibly new content and destroy the html_button that triggered an event
+	ignore_events = true;
+
+	try
+	{
+	    ret = html_popup::inherited_get_body_part(path, req);
+	}
+	catch(...)
+	{
+	    ignore_events = false;
+	    throw;
+	}
+	ignore_events = false;
+
+	warning.clear();
+	return ret;
+    }
+    else
+	return "";
+}
+
+void html_select_file::new_css_library_available()
+{
+    unique_ptr<css_library> & csslib = lookup_css_library();
+    css tmp;
+
+    if(!csslib)
+	throw WEBDAR_BUG;
+
+    webdar_css_style::update_library(*csslib);
+
+    tmp.clear();
+
+    tmp.css_float(css::fl_right);
+    if(!csslib->class_exists(css_float_button_right))
+	csslib->add(css_float_button_right, tmp);
+
+    tmp.clear();
+
+    tmp.css_float(css::fl_left);
+    if(!csslib->class_exists(css_float_button_left))
+	csslib->add(css_float_button_left, tmp);
+
+    tmp.clear();
+
+    tmp.css_font_weight_bold();
+    tmp.css_color("rgb(255,30,10)");
+    if(!csslib->class_exists(css_warning))
+	csslib->add(css_warning, tmp);
+
+    tmp.clear();
+
+    tmp.css_position_type(css::pos_sticky);
+    tmp.css_box_sizing(css::bx_border);
+    tmp.css_position_top("0");
+    tmp.css_border_style(css::bd_bottom, css::bd_solid);
+    tmp.css_border_width(css::bd_bottom, css::bd_thin);
+    tmp.css_margin("0");
+    tmp.css_padding("10px");
+    tmp.css_padding_bottom("10px");
+    tmp.css_width("100%", true);
+    tmp.css_background_color("white");
+    if(!csslib->class_exists(css_sticky_top))
+	csslib->add(css_sticky_top, tmp);
+
+    tmp.clear();
+
+    tmp.css_position_type(css::pos_sticky);
+    tmp.css_box_sizing(css::bx_border);
+    tmp.css_float(css::fl_left);
+    tmp.css_position_bottom("0");
+    tmp.css_border_style(css::bd_top, css::bd_solid);
+    tmp.css_border_width(css::bd_top, css::bd_thin);
+    tmp.css_margin("0");
+    tmp.css_padding("10px");
+    tmp.css_width("100%", true);
+    tmp.css_background_color("white");
+    if(!csslib->class_exists(css_sticky_bot))
+	csslib->add(css_sticky_bot, tmp);
+
+}
+
+void html_select_file::fill_content()
+{
+    string entry;
+    string event_name;
+    unsigned int count = 0;
+
+    clear_content();
+
+    if(!entr)
+	throw WEBDAR_BUG;
+
+    try
+    {
+	entr->set_location(fieldset.get_label());
+	entr->read_dir_reset();
+
+	while(entr->read_dir_next(entry))
+	{
+	    event_name = "x_" + to_string(count++);
+	    if(listed.find(event_name) != listed.end())
+		throw WEBDAR_BUG; // event already exists!?!
+
+		// for new we lack from entrepot the ability to know whether the entry is a directory or not
+	    content.adopt_static_html("?");
+
+	    html_button* btn_entry = new (nothrow) html_button(entry, event_name);
+	    if(btn_entry == nullptr)
+		throw exception_memory();
+
+	    listed[event_name] = btn_entry;
+	    content.adopt(btn_entry);
+	    btn_entry->record_actor_on_event(this, event_name);
+	}
+    }
+    catch(libdar::Ebug & e)
+    {
+	throw;
+    }
+    catch(libdar::Egeneric & e)
+    {
+	warning.clear();
+	warning.add_text(3, e.get_message());
+    }
+}
+
+void html_select_file::clear_content()
+{
+    map<string, html_button*>::iterator it = listed.begin();
+
+    content.clear();
+    while(it != listed.end())
+    {
+	if(it->second != nullptr)
+	{
+	    delete it->second;
+	    it->second = nullptr;
+	}
+
+	++it;
+    }
+    listed.clear();
+}
---
+++ src/html_select_file.hpp
@@ -0,0 +1,163 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_SELECT_FILE_HPP
+#define HTML_SELECT_FILE_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include <map>
+#include <dar/libdar.hpp>
+#include <memory>
+
+    // webdar headers
+#include "events.hpp"
+#include "actor.hpp"
+#include "html_div.hpp"
+#include "html_text.hpp"
+#include "html_form_fieldset.hpp"
+#include "html_button.hpp"
+#include "html_table.hpp"
+#include "html_popup.hpp"
+
+    /// \file html_select_file.hpp defines html_select_file class
+    ///
+    /// given a libdar::entrepot object, the class html_select_file
+    /// shows a popup for the user to navigate in the directory tree
+    /// and select a file or a directory depending on the parameter set
+    /// to this object
+    ///
+    /// usage for the caller is to:
+    ///- register for entry_selected and op_cancelled events
+    /// on this object, events which will be triggered once the user will
+    /// have selected a file or cancelled choice selection
+    ///- call the different set_* methods
+    ///- then call run() giving a libdar::entrepot
+    ///- and upon event entry_selected fetch the path selected by the user
+    ///  calling get_selected_path()
+
+class html_select_file: public html_popup, public events, public actor
+{
+public:
+	/// event when user selected a file
+    static const std::string entry_selected;
+
+	/// event when the user cancelled file selection
+    static const std::string op_cancelled;
+
+	/// constructor
+
+	/// \param[in] message is show to the user and should tell what it is expected to select for
+    html_select_file(const std::string & message);
+
+    html_select_file(const html_select_file & ref) = delete;
+    html_select_file(html_select_file && ref) noexcept(false) = delete;
+    html_select_file & operator = (const html_select_file & ref) = delete;
+    html_select_file & operator = (html_select_file && ref) noexcept(false) = delete;
+    virtual ~html_select_file() { clear_content(); };
+
+	/// set the current dir before read
+    void set_starting_directory(const std::string & val) { if(status != st_init) throw WEBDAR_BUG; fieldset.change_label(val); };
+    void set_only_dir(bool val) { if(status != st_init) throw WEBDAR_BUG; only_dir = val; };
+    void set_can_create_dir(bool val) { if(status != st_init) throw WEBDAR_BUG; btn_createdir.set_visible(val); };
+
+	/// start the user interaction for a path selection
+
+	/// \param[in] x_entr entrepot to scan directory from
+	/// \param[in] absolute path in x_entr to take as starting directory
+	/// \note if start_dir is not a directory not is an absolute path
+	/// an exception is thrown
+    void run(const std::shared_ptr<libdar::entrepot> & x_entr,
+	     const std::string & start_dir);
+
+	/// obtain the path selected by the user (mandatory after entry_selected event, in order to reuse this object)
+    std::string get_selected_path() const { if(status != st_completed) throw WEBDAR_BUG; status = st_init; return fieldset.get_label(); };
+
+	/// action triggered from html_buttons internally used in this class implementation (do not use, even if it must stay a public method)
+    virtual void on_event(const std::string & event_name) override;
+
+protected:
+
+	/// inherited from body_builder (via html_div)
+    virtual std::string inherited_get_body_part(const chemin & path,
+						const request & req) override;
+
+	/// inherited from body_builder (via html_div)
+    virtual void new_css_library_available() override;
+
+private:
+	// event nam for the 'change to parent directory' button (parentdir)
+    static const std::string op_chdir_parent;
+    static const std::string op_createdir;
+
+	// internal css class names
+    static const std::string css_float_button_right;
+    static const std::string css_float_button_left;
+    static const std::string css_warning;
+    static const std::string css_sticky_top;
+    static const std::string css_sticky_bot;
+
+	// popup size of browser windows
+    static constexpr const unsigned int width_pct = 80;
+    static constexpr const unsigned int height_pct = 80;
+
+	// object status
+
+    mutable enum { st_init, st_run, st_completed } status;
+
+	// settings
+
+    bool only_dir;
+    std::shared_ptr<libdar::entrepot> entr;
+
+
+	// html components
+
+    html_text title;                  ///< shows the message request for the user (provided at constructor time)
+    html_text warning;                ///< shows warnings when needed (permission pb, etc.)
+    html_div title_box;               ///< contains title and warning and stay visible as a sticky box
+    html_form_fieldset fieldset;      ///< shows the current directory path
+    html_button parentdir;            ///< change to parent dir
+    html_table content;               ///< parent of content objects
+    std::map<std::string, html_button*> listed; ///< associate a event message to each listed items
+    html_div btn_box;                 ///< box containing the bottom buttons
+    html_button btn_cancel;           ///< triggers the entry_cancelled event
+    html_button btn_validate;         ///< trigger the entry_selected event
+    html_button btn_createdir;        ///< leads to create a new directory
+    bool ignore_events;               ///< in inherited_get_body_part() we trigger twice the adopted objects but must take action on events only once
+
+	// internal routines
+
+	/// fills content and listed from entry located in fieldset.get_label() directory of the entr libdar::entrepot
+    void fill_content();
+
+	/// free up and clean all html_button of listed and clear content too in coherence
+    void clear_content();
+};
+
+#endif
