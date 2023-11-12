0d353589e67f4041d3f94b9a6b65f3ed2337efcf
Denis corbin <dar.linux@free.fr>
2023-11-12 20:38:32 +0100

    adding class html_form_input_file

---
+++ src/html_form_input_file.cpp
@@ -0,0 +1,114 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "webdar_css_style.hpp"
+#include "webdar_css_style.hpp"
+
+    //
+#include "html_form_input_file.hpp"
+
+using namespace std;
+
+const string html_form_input_file::changed_event = "html_form_input_file_changed";
+const string html_form_input_file::triggered_event = "triggered";
+
+html_form_input_file::html_form_input_file(const string & label,
+					   const string & initial_value,
+					   unsigned int size,
+					   const string & popup_message):
+    input(label, html_form_input::text, initial_value, size),
+    trigger("+", triggered_event),
+    user_select(popup_message),
+    changed_event_name(changed_event),
+    entrep(nullptr)
+{
+	// html adoption tree
+
+    adopt(&input);
+    adopt(&trigger);
+    adopt(&user_select);
+
+	// cabling events
+
+    input.record_actor_on_event(this, html_form_input::changed);
+    trigger.record_actor_on_event(this, triggered_event);
+    user_select.record_actor_on_event(this, html_select_file::entry_selected);
+
+	// css classes
+
+    webdar_css_style::normal_button(trigger);
+
+    entrep.reset(new (nothrow) libdar::entrepot_local("", "", false));
+    if(!entrep)
+	throw exception_memory();
+}
+
+
+void html_form_input_file::set_change_event_name(const string & name)
+{
+    register_name(name);
+    changed_event_name = name;
+}
+
+void html_form_input_file::set_entrepot(const shared_ptr<libdar::entrepot> & entrepot)
+{
+    if(!entrepot)
+	throw WEBDAR_BUG;
+    entrep = entrepot;
+}
+
+void html_form_input_file::on_event(const string & event_name)
+{
+    if(event_name == "html_form_input::changed")
+	act(changed_event_name);
+    else if(event_name == triggered_event)
+	user_select.run(entrep, input.get_value());
+    else if(event_name == "html_select_file::entry_selected")
+    {
+	input.set_value(user_select.get_selected_path());
+
+
+    }
+    else
+	throw WEBDAR_BUG;
+}
+
+void html_form_input_file::new_css_library_available()
+{
+    unique_ptr<css_library> & csslib = lookup_css_library();
+
+    if(!csslib)
+	throw WEBDAR_BUG;
+
+    webdar_css_style::update_library(*csslib);
+}
---
+++ src/html_form_input_file.hpp
@@ -0,0 +1,108 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_FORM_INPUT_FILE_HPP
+#define HTML_FORM_INPUT_FILE_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include <memory>
+
+    // webdar headers
+#include "html_div.hpp"
+#include "events.hpp"
+#include "html_form_input.hpp"
+#include "html_button.hpp"
+#include "html_select_file.hpp"
+
+    /// class html_form_input_file provides a mean to the user to navigate to filesystem to select a file or directory
+
+    /// this is an html_form_input with an added button
+    /// that triggers a popup for the user to select a file
+    /// or a directory in a given libdar::entrepot (or by
+    /// default an libdar::entrepot_local objeect).
+    /// user can also manually modify the entry in the <INPUT>
+    /// as a normal html_form_input object adopted by an
+    /// html_form
+
+class html_form_input_file: public html_div, public events, public actor
+{
+public:
+
+    static const std::string changed_event; ///< name of the change event unless changed
+
+    html_form_input_file(const std::string & label,
+			 const std::string & initial_value,
+			 unsigned int size,
+			 const std::string & popup_message);
+
+    html_form_input_file(const html_form_input_file & ref) = default;
+    html_form_input_file(html_form_input_file && ref) noexcept = default;
+    html_form_input_file & operator = (const html_form_input_file & ref) = default;
+    html_form_input_file & operator = (html_form_input_file && ref) noexcept = default;
+    ~html_form_input_file() = default;
+
+    	// relayed from html_form_input
+
+    void change_label(const std::string & label) { input.change_label(label); };
+    const std::string & get_value() const { return input.get_value(); };
+    void set_value(const std::string & val) { input.set_value(val); };
+
+	/// change the name of the "changed" event
+    void set_change_event_name(const std::string & name);
+
+	// relayed from html_select_file
+
+    void set_select_dir(bool val) { user_select.set_select_dir(val); };
+    void set_can_create_dir(bool val) { user_select.set_can_create_dir(val); };
+
+	/// change the entrepot to search into (by default this is local filesystem
+    void set_entrepot(const std::shared_ptr<libdar::entrepot> & entrepot);
+
+	// inherited from events
+
+    virtual void on_event(const std::string & event_name) override;
+
+protected:
+
+	// inherited from body_builder
+    virtual void new_css_library_available() override;
+
+
+private:
+    html_form_input input;
+    html_button trigger;
+    html_select_file user_select;
+    std::string changed_event_name;
+    std::shared_ptr<libdar::entrepot> entrep;
+
+    static const std::string triggered_event;
+};
+
+
+#endif
