ec0d76cef1b9c69752cbcef04655d67a0e97c97f
Denis corbin <dar.linux@free.fr>
2024-01-03 14:36:02 +0100

    adding class html_entrepot
    missing implementation of initial value for sftp stuff based on environment variable

---
+++ src/html_entrepot.cpp
@@ -0,0 +1,213 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include "webdar_tools.hpp"
+
+    // webdar headers
+
+
+    //
+#include "html_entrepot.hpp"
+
+using namespace std;
+
+html_entrepot::html_entrepot():
+    form("Apply changes"),
+    fs("Entrepot parameters"),
+    repo_type("Entrepot type"),
+    host("Remote host", html_form_input::text, "", 30),
+    port("Port", html_form_input::number, "0", 7),
+    login("Login", html_form_input::text, "", 30),
+    auth_type("Authentication mode"),
+    pass("Password", html_form_input::password, "", 30),
+    auth_from_file("Fetch password from netrc file", html_form_input::check, "1", 1),
+    known_hosts_file("Known-host file", "/", 40, "Select the knowhosts file..."),
+    wait_time("Network retry delay (s)", html_form_input::number, "30", 5),
+    verbose("Verbose network connection", html_form_input::check, "", 1)
+{
+    got_inner_event = false;
+
+	// component configuration
+    repo_type.add_choice(type_local, "local filesystem");
+    repo_type.add_choice(type_ftp, "FTP protocol (unciphered)");
+    repo_type.add_choice(type_sftp, "SFTP protocol (ciphered)");
+    repo_type.set_selected(0);
+
+    auth_type.add_choice(auth_pass, "Password authentication");
+    auth_type.add_choice(auth_key, "Keyfile authentication");
+    auth_type.set_selected(0);
+
+    	// adoption tree
+    fs.adopt(&repo_type);
+    fs.adopt(&host);
+    fs.adopt(&port);
+    fs.adopt(&login);
+    fs.adopt(&auth_type);
+    fs.adopt(&pass);
+    fs.adopt(&auth_from_file);
+    fs.adopt(&known_hosts_file);
+    fs.adopt(&wait_time);
+    fs.adopt(&verbose);
+    form.adopt(&fs);
+    adopt(&form);
+
+	// events and actors
+    repo_type.record_actor_on_event(this, html_form_select::changed);
+    auth_type.record_actor_on_event(this, html_form_select::changed);
+
+	// css if needed
+
+	// update components visibility
+    update_visible();
+}
+
+
+shared_ptr<libdar::entrepot> html_entrepot::get_entrepot(const shared_ptr<libdar::user_interaction> & dialog) const
+{
+    shared_ptr<libdar::entrepot> ret;
+    libdar::mycurl_protocol proto;
+
+    switch(repo_type.get_selected_num())
+    {
+    case 0:
+	ret.reset(new (nothrow) libdar::entrepot_local("", "", true));
+	break;
+    case 1:
+    case 2:
+	switch(repo_type.get_selected_num())
+	{
+	case 1:
+	    proto = libdar::proto_ftp;
+	    break;
+	case 2:
+	    proto = libdar::proto_sftp;
+	    break;
+	default:
+	    throw WEBDAR_BUG;
+	}
+
+	ret.reset(new (nothrow) libdar::entrepot_libcurl(dialog,
+							 proto,
+							 login.get_value(),
+							 libdar::secu_string(pass.get_value().c_str(), pass.get_value().size()),
+							 host.get_value(),
+							 port.get_value(),
+							 auth_from_file.get_value_as_bool(),
+							 string("/root/.ssh/id_rsa.pub"), ///<< to review with env variables
+							 string("/root/.ssh/id_rsa"), ///< to review with env variables
+							 known_hosts_file.get_value(),
+							 libdar::U_I(webdar_tools_convert_to_int(wait_time.get_value())),
+							 verbose.get_value_as_bool()));
+	break;
+    default:
+	throw WEBDAR_BUG;
+    }
+
+    if(!ret)
+	throw exception_memory();
+
+    return ret;
+}
+
+
+void html_entrepot::on_event(const std::string & event_name)
+{
+    got_inner_event = true;
+    update_visible();
+}
+
+std::string html_entrepot::inherited_get_body_part(const chemin & path,
+						   const request & req)
+{
+    got_inner_event = false;
+    string ret = get_body_part_from_all_children(path, req);
+
+    if(got_inner_event) // need get the update body part as some changed occured
+    {
+	got_inner_event = false;
+	ret = get_body_part_from_all_children(path, req);
+	if(got_inner_event)
+	    throw WEBDAR_BUG; // not expected to have changed event after visibility change only
+    }
+
+    return ret;
+}
+
+void html_entrepot::update_visible()
+{
+    switch(repo_type.get_selected_num())
+    {
+    case 0: // local
+	host.set_visible(false);
+	port.set_visible(false);
+	login.set_visible(false);
+	auth_type.set_visible(false);
+	pass.set_visible(false);
+	auth_from_file.set_visible(false);
+	known_hosts_file.set_visible(false);
+	wait_time.set_visible(false);
+	verbose.set_visible(false);
+	break;
+    case 1: // ftp
+    case 2: // sftp
+	host.set_visible(true);
+	port.set_visible(true);
+	login.set_visible(true);
+	if(repo_type.get_selected_num() == 1) // ftp
+	{
+	    auth_type.set_visible(false);
+	    auth_from_file.set_visible(false);
+	    known_hosts_file.set_visible(false);
+	}
+	else // sftp
+	{
+	    auth_type.set_visible(true);
+	    switch(auth_type.get_selected_num())
+	    {
+	    case 0: // pass
+		pass.set_visible(true);
+		auth_from_file.set_visible(true);
+		break;
+	    case 1: // auth key
+		pass.set_visible(false);
+		auth_from_file.set_visible(false);
+		break;
+	    default:
+		throw WEBDAR_BUG;
+	    }
+	    known_hosts_file.set_visible(true);
+	}
+	wait_time.set_visible(true);
+	verbose.set_visible(true);
+	break;
+    default:
+	throw WEBDAR_BUG;
+    }
+}
---
+++ src/html_entrepot.hpp
@@ -0,0 +1,94 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_ENTREPOT_HPP
+#define HTML_ENTREPOT_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "body_builder.hpp"
+#include "html_form.hpp"
+#include "html_form_input.hpp"
+#include "html_form_input_file.hpp"
+#include "html_form_fieldset.hpp"
+#include "html_form_select.hpp"
+#include "actor.hpp"
+
+    /// class html_entrepot let user define an entrepot parameters
+
+class html_entrepot: public body_builder, public actor
+{
+public:
+    html_entrepot();
+    html_entrepot(const html_entrepot & ref) = delete;
+    html_entrepot(html_entrepot && ref) noexcept = delete;
+    html_entrepot & operator = (const html_entrepot & ref) = delete;
+    html_entrepot & operator = (html_entrepot && ref) noexcept = delete;
+    ~html_entrepot() = default;
+
+	// available fields for libdar
+    std::shared_ptr<libdar::entrepot> get_entrepot(const std::shared_ptr<libdar::user_interaction> & dialog) const;
+
+	// inherited from actor parent class
+    virtual void on_event(const std::string & event_name) override;
+
+protected:
+	/// inherited from body_builder
+    virtual std::string inherited_get_body_part(const chemin & path,
+						const request & req) override;
+
+private:
+    static constexpr const char* type_local = "local";
+    static constexpr const char* type_ftp   = "ftp";
+    static constexpr const char* type_sftp  = "sftp";
+    static constexpr const char* auth_pass = "password";
+    static constexpr const char* auth_key  = "keyfile";
+
+    bool got_inner_event; // set to true by on_event()
+
+    html_form form;
+    html_form_fieldset fs;
+    html_form_select repo_type; // local, ftp or sftp
+    html_form_input host;      // ftp and sftp
+    html_form_input port;      // ftp and sftp
+    html_form_input login;     // ftp and sftp
+    html_form_select auth_type; // (sftp only) login or keyfile
+    html_form_input pass;      // ftp and sftp
+    html_form_input auth_from_file;  // ftp and sftp
+    html_form_input_file known_hosts_file; // sftp only
+    html_form_input wait_time; // ftp and sftp
+    html_form_input verbose;   // ftp and sftp
+
+    void update_visible();
+
+};
+
+#endif
