3d341a6a19058961a92b48b84788a8386e9e176a
Denis corbin <dar.linux@free.fr>
2024-03-13 21:38:25 +0100

    adding class html_libdar_running_popup

---
+++ src/html_libdar_running_popup.cpp
@@ -0,0 +1,101 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "html_page.hpp"
+
+    //
+#include "html_libdar_running_popup.hpp"
+
+using namespace std;
+
+const string html_libdar_running_popup::libdar_has_finished = "html_libdar_running_popup_finished";
+
+
+html_libdar_running_popup::html_libdar_running_popup():
+    html_popup(90,90),
+    enable_refresh(false),
+    changed_refresh(false)
+{
+    web_ui.reset(new (nothrow) html_web_user_interaction());
+    if(!web_ui)
+	throw exception_memory();
+
+    adopt(web_ui.get());
+
+	// signals and events
+    register_name(libdar_has_finished);
+
+    web_ui->record_actor_on_event(this, html_web_user_interaction::libdar_has_finished);
+    web_ui->record_actor_on_event(this, html_web_user_interaction::can_refresh);
+    web_ui->record_actor_on_event(this, html_web_user_interaction::dont_refresh);
+}
+
+string html_libdar_running_popup::inherited_get_body_part(const chemin & path,
+						    const request & req)
+{
+    if(changed_refresh)
+    {
+	html_page* page = nullptr;
+
+	closest_ancestor_of_type(page);
+	changed_refresh = false;
+
+	if(page != nullptr)
+	{
+	    if(enable_refresh)
+		page->set_refresh_redirection(1, req.get_uri().get_path().display(false));
+	    else
+		page->set_refresh_redirection(0, ""); // disable refresh
+	}
+    }
+
+    return get_body_part_from_all_children(path, req);
+}
+
+void html_libdar_running_popup::on_event(const std::string & event_name)
+{
+    if(event_name == html_web_user_interaction::libdar_has_finished)
+	act(libdar_has_finished); // propagating the event
+    else if(event_name == html_web_user_interaction::can_refresh)
+    {
+	enable_refresh = true;
+	changed_refresh = true;
+    }
+    else if(event_name == html_web_user_interaction::dont_refresh)
+    {
+	enable_refresh = false;
+	changed_refresh = true;
+    }
+    else
+	throw WEBDAR_BUG;
+}
---
+++ src/html_libdar_running_popup.hpp
@@ -0,0 +1,86 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2023 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_LIBDAR_RUNNING_POPUP_HPP
+#define HTML_LIBDAR_RUNNING_POPUP_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+    // webdar headers
+#include "html_popup.hpp"
+#include "events.hpp"
+#include "actor.hpp"
+#include "html_web_user_interaction.hpp"
+
+    /// html_libdar_running_popup wraps up an html_web_user_interaction in a html_page
+
+    /// also handle page title and for that need to know the session name
+
+class html_libdar_running_popup : public html_popup, public events, public actor
+{
+public:
+    	// this class generates the following events:
+    static const std::string libdar_has_finished; ///< libdar execution has ended and user has acknoledged it
+
+	// constructor
+    html_libdar_running_popup();
+    html_libdar_running_popup(const html_libdar_running_popup & ref) = delete;
+    html_libdar_running_popup(html_libdar_running_popup && ref) noexcept = delete;
+    html_libdar_running_popup & operator = (const html_libdar_running_popup & ref) = delete;
+    html_libdar_running_popup & operator = (html_libdar_running_popup && ref) noexcept = delete;
+    ~html_libdar_running_popup() = default;
+
+	/// inherited from actor
+    virtual void on_event(const std::string & event_name) override;
+
+	/// propagate request to web_ui
+    std::shared_ptr<html_web_user_interaction> get_html_user_interaction() { return web_ui; };
+
+	/// propagate request to web_ui
+    void run_and_control_thread(libthreadar::thread* arg) { web_ui->run_and_control_thread(arg); };
+
+	/// whether a libdar thread is running under "this" management
+    bool is_libdar_running() const { return web_ui->is_libdar_running(); };
+
+	/// whether libdar thread has been aborted (to be checked by the caller upon libdar_has_finished event)
+    bool has_libdar_been_aborted() const { return web_ui->has_libdar_been_aborted(); };
+
+protected:
+	/// inherited from body_builder
+    virtual std::string inherited_get_body_part(const chemin & path,
+						const request & req) override;
+
+private:
+    std::shared_ptr<html_web_user_interaction> web_ui;
+    bool enable_refresh;    ///< refresh mode status
+    bool changed_refresh;   ///< whether page refresh has been modified
+
+};
+
+#endif
