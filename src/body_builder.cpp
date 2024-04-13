Edrusb/webdar

commit 227dcdeafdacf530b72289895e2e51dfb1000958

fixing bug in body_builder class

src/body_builder.cpp
@@ -335,6 +335,8 @@ string body_builder::get_body_part(const chemin & path,
 
 	do
 	{
+	    body_changed = false;
+
 	    ret = get_body_part_or_cache(path, localreq);
 
 	    if(body_changed)
@@ -573,7 +575,6 @@ string body_builder::get_body_part_or_cache(const chemin & path,
 	ret = last_body_part;
     else
     {
-	body_changed = false;
 	ret = inherited_get_body_part(path, req);
 	last_body_path = path;
 	last_body_req_uri = req.get_uri();

commit 1304b44ebcc59f53841b4f98dcaba66eb6037eb2

fixed bug introduced with commit 227dcdeafdacf530b72289895e2e51dfb1000958 in body_builder class

src/body_builder.cpp
@@ -335,13 +335,14 @@ string body_builder::get_body_part(const chemin & path,
 
 	do
 	{
-	    body_changed = false;
-
 	    ret = get_body_part_or_cache(path, localreq);
 
 	    if(body_changed)
+	    {
 		localreq.post_to_get();
 		// we avoid regenerating post events
+		body_changed = false;
+	    }
 
 	    if(++loop >= maxloop)
 		throw WEBDAR_BUG;