Edrusb/webdar

commit a85e2e28b64b18e8548aa755e1f1575c4fd058f4

adding html_form_input::get_event_name() method

src/html_form_input.hpp
@@ -78,6 +78,7 @@ public:
 
 	/// change the name of the event generated when this control changes
     void set_change_event_name(const std::string & name);
+    std::string get_event_name() const { modif_changed.empty()? changed: modif_changed; };
 
 protected:
 	/// inherited from body_builder

commit eed3f3edd55fad659620c3bb77fbc43070359bf8

fixing compilation bug in html_form_input introduced by commit a85e2e28b64b18e8548aa755e1f1575c4fd058f4

src/html_form_input.hpp
@@ -78,7 +78,7 @@ public:
 
 	/// change the name of the event generated when this control changes
     void set_change_event_name(const std::string & name);
-    std::string get_event_name() const { modif_changed.empty()? changed: modif_changed; };
+    std::string get_event_name() const { return modif_change.empty()? changed: modif_change; };
 
 protected:
 	/// inherited from body_builder