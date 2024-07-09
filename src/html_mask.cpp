f33d168850e2983b2303a5a93f04e75dc8d45dfd
Denis corbin <dar.linux@free.fr>
2024-07-09 23:40:07 +0200

    adding html_mask.cpp that was missing in git

---
+++ src/html_mask.cpp
@@ -0,0 +1,58 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "tokens.hpp"
+
+
+    //
+#include "html_mask.hpp"
+
+using namespace std;
+
+void html_mask::new_css_library_available()
+{
+    unique_ptr<css_library> & csslib = lookup_css_library();
+    if(!csslib)
+	throw WEBDAR_BUG;
+
+    if(! csslib->class_exists(red_border))
+    {
+	css tmp;
+
+	tmp.clear();
+	tmp.css_border_width(css::bd_all, css::bd_thick, false);
+	tmp.css_border_color(css::bd_all, RED);
+
+	csslib->add(red_border, tmp);
+    }
+}
