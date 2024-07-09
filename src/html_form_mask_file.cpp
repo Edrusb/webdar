9dca2486cfb42ba3d60fbdcf1a08a0529a358f2f
Denis corbin <dar.linux@free.fr>
2024-07-09 21:10:39 +0200

    adding html_form_mask_file class

---
+++ src/html_form_mask_file.cpp
@@ -0,0 +1,104 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include <dar/libdar.hpp>
+
+    // webdar headers
+
+
+
+    //
+#include "html_form_mask_file.hpp"
+
+using namespace std;
+
+#define FILENAME_INIT filename("File name", "/", 30, "Select a listing text file...")
+
+html_form_mask_file::html_form_mask_file():
+    FILENAME_INIT,
+    exclude_checkbox("Exclude listed files",
+		     html_form_input::check,
+		     "", // unchecked
+		     1),
+    casesensit("Case sensitivivity",
+	       html_form_input::check,
+	       "1", // checked
+	       1),
+    fs("File listing"),
+    prefix(libdar::FAKE_ROOT)
+{
+    init();
+}
+
+html_form_mask_file::html_form_mask_file(const html_form_mask_file & ref):
+    FILENAME_INIT,
+    exclude_checkbox(ref.exclude_checkbox),
+    casesensit(ref.casesensit),
+    fs(ref.fs),
+    prefix(ref.prefix)
+{
+
+    init();
+}
+
+unique_ptr<libdar::mask> html_form_mask_file::get_mask() const
+{
+    deque<string> empty_list;
+    unique_ptr<libdar::mask> tmp(new (nothrow) libdar::mask_list(
+				     filename.get_value(),
+				     casesensit.get_value_as_bool(),
+				     prefix,
+				     ! exclude_checkbox.get_value_as_bool(),
+				     empty_list));
+
+    if(!tmp)
+	throw exception_memory();
+
+    return tmp;
+}
+
+string html_form_mask_file::inherited_get_body_part(const chemin & path,
+						    const request & req)
+{
+    return get_body_part_from_all_children(path, req);
+}
+
+
+
+
+void html_form_mask_file::init()
+{
+	// adoption tree
+
+    fs.adopt(&filename);
+    fs.adopt(&exclude_checkbox);
+    fs.adopt(&casesensit);
+    adopt(&fs);
+}
---
+++ src/html_form_mask_file.hpp
@@ -0,0 +1,101 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_FORM_MASK_FILE_HPP
+#define HTML_FORM_MASK_FILE_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+    // webdar headers
+#include "html_mask.hpp"
+#include "html_form_input.hpp"
+#include "html_form_fieldset.hpp"
+#include "html_form_input_file.hpp"
+
+    /// class html_form_mask_file provide interface to include or exclude a list of entry taken from a text file
+
+    /// this html components is consitued of:
+    ///
+    /// +----html_fieldset----------------------------------+
+    /// |                                                   |
+    /// |          +-----------------------------+          |
+    /// | filename |                             |          |
+    /// |          +-----------------------------+          |
+    /// |                                                   |
+    /// | exclude checkbox : +-+                            |
+    /// |                    | |                            |
+    /// |                    +-+                            |
+    /// | casesensit       : +-+                            |
+    /// |                    | |                            |
+    /// |                    +-+                            |
+
+    /// |                                                   |
+    /// +---------------------------------------------------+
+    ///
+    /// \note this component includes an html_form by default
+    /// (see constructor argument), but should not when used
+    /// recursively from another html_form_mask_file
+
+
+class html_form_mask_file : public html_mask
+{
+public:
+    html_form_mask_file();
+    html_form_mask_file(const html_form_mask_file & ref);
+    html_form_mask_file(html_form_mask_file && ref) noexcept = delete;
+    html_form_mask_file & operator = (const html_form_mask_file & ref) = delete;
+    html_form_mask_file & operator = (html_form_mask_file && ref) noexcept = delete;
+    ~html_form_mask_file() = default;
+
+    	/// inherited from html_mask
+    virtual void set_root_prefix(const libdar::path & x_prefix) override { prefix = x_prefix; };
+
+	/// inherited from html_mask
+    virtual std::unique_ptr<libdar::mask> get_mask() const override;
+
+	/// clone() implementation
+    MASK_CLONER_MACRO;
+
+protected:
+	/// inherited methods from body_builder
+    virtual std::string inherited_get_body_part(const chemin & path,
+						const request & req) override;
+
+
+private:
+    html_form_input_file filename;
+    html_form_input exclude_checkbox;
+    html_form_input casesensit;
+    html_form_fieldset fs;
+    libdar::path prefix;
+
+    void init();
+};
+
+#endif
