0132030d4ef56eaa415ce66472db448ca181a2db
Denis corbin <dar.linux@free.fr>
2024-09-02 15:02:26 +0200

    adding class html_form_overwrite_action

---
+++ src/html_form_overwrite_action.cpp
@@ -0,0 +1,134 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "html_form_overwrite_constant_action.hpp"
+#include "html_form_overwrite_conditional_action.hpp"
+
+    //
+#include "html_form_overwrite_action.hpp"
+
+using namespace std;
+
+html_form_overwrite_action::html_form_overwrite_action(const string & label):
+    fs(label),
+    action_type("Action type",  act_changed)
+{
+
+	// components setup
+    action_type.add_choice(action_type_const, "constant action");
+    action_type.add_choice(action_type_condition, "conditional action");
+    action_type.set_selected(0);
+    action.reset(new (nothrow) html_form_overwrite_constant_action());
+    if(!action)
+	throw exception_memory();
+    old_action.reset();
+
+	// adoption tree
+    fs.adopt(&action_type);
+    fs.adopt(&(*action));
+    adopt(&fs);
+
+	// events
+    action_type.record_actor_on_event(this, act_changed);
+
+	// css
+}
+
+unique_ptr<libdar::crit_action> html_form_overwrite_action::get_overwriting_action() const
+{
+    unique_ptr<libdar::crit_action> ret;
+
+    if(!action)
+	throw WEBDAR_BUG;
+
+    ret = action->get_overwriting_action();
+    if(!ret)
+	throw WEBDAR_BUG;
+
+    return ret;
+}
+
+void html_form_overwrite_action::on_event(const std::string & event_name)
+{
+    if(event_name == act_changed)
+    {
+	if(old_action)
+	    throw WEBDAR_BUG;
+	if(changed)
+	    throw WEBDAR_BUG;
+
+	changed = true;
+	old_action = std::move(action);
+
+	if(action_type.get_selected_id() == action_type_const)
+	{
+	    action.reset(new (nothrow) html_form_overwrite_constant_action());
+	    if(!action)
+		throw exception_memory();
+		// adoption of this new object will be done in inherited_get_body_part()
+	}
+	else if(action_type.get_selected_id() == action_type_condition)
+	{
+	    action.reset(new (nothrow) html_form_overwrite_conditional_action());
+	    if(!action)
+		throw exception_memory();
+		// adoption of this new object will be done in inherited_get_body_part()
+	}
+	else // unknown action type
+	    throw WEBDAR_BUG;
+    }
+    else // unexpected event name
+	throw WEBDAR_BUG;
+}
+
+string html_form_overwrite_action::inherited_get_body_part(const chemin & path,
+							   const request & req)
+{
+    string ret;
+
+    changed = false;
+
+    ret = get_body_part_from_all_children(path, req);
+
+    if(changed)
+    {
+	old_action.reset();
+	if(action)
+	    fs.adopt(&(*action));
+	else
+	    throw WEBDAR_BUG;
+    }
+
+    return ret;
+}
+
---
+++ src/html_form_overwrite_action.hpp
@@ -0,0 +1,79 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_FORM_OVERWRITE_ACTION_HPP
+#define HTML_FORM_OVERWRITE_ACTION_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+    // webdar headers
+#include "html_overwrite_action.hpp"
+#include "body_builder.hpp"
+#include "html_form_select.hpp"
+#include "html_form_fieldset.hpp"
+#include "actor.hpp"
+
+    /// html component used for constant actions in overwriting policies
+
+class html_form_overwrite_action: public html_overwrite_action, public actor
+{
+public:
+    html_form_overwrite_action(const std::string & label);
+    html_form_overwrite_action(const html_form_overwrite_action & ref) = default;
+    html_form_overwrite_action(html_form_overwrite_action && ref) noexcept = default;
+    html_form_overwrite_action & operator = (const html_form_overwrite_action & ref) = default;
+    html_form_overwrite_action & operator = (html_form_overwrite_action && ref) noexcept = default;
+    ~html_form_overwrite_action() = default;
+
+	/// obtain the crit_action object for libdar option
+    virtual std::unique_ptr<libdar::crit_action> get_overwriting_action() const override;
+
+    	/// inherited from actor
+    virtual void on_event(const std::string & event_name) override;
+
+protected:
+
+	/// inherited from body_builder from html_overwrite_action
+    virtual std::string inherited_get_body_part(const chemin & path,
+						const request & req) override;
+
+private:
+    static constexpr const char* act_changed = "action changed";
+
+    static constexpr const char* action_type_const = "constant";
+    static constexpr const char* action_type_condition = "condition";
+
+    html_form_fieldset fs;
+    html_form_select action_type;
+    std::unique_ptr<html_overwrite_action> action;
+    std::unique_ptr<html_overwrite_action> old_action; ///< to keep action existing when changed from on_event()
+    bool changed;
+};
+
+#endif
