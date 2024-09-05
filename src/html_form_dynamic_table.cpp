73065b3215b85e9400ccddf65ea41bbd552e1a96
Denis corbin <dar.linux@free.fr>
2024-09-05 20:23:07 +0200

    adding class html_form_dynamic_table

---
+++ src/html_form_dynamic_table.cpp
@@ -0,0 +1,194 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "webdar_tools.hpp"
+
+
+    //
+#include "html_form_dynamic_table.hpp"
+
+using namespace std;
+
+
+html_form_dynamic_table::html_form_dynamic_table(bool has_left_labels,
+						 bool selector_below,
+						 const string & adder_label,
+						 const string & adder_default_choice):
+    left_label(has_left_labels),
+    table(has_left_labels ? 3 : 2),
+    adder(adder_label, new_line_to_add)
+{
+	// components configuration/
+    adder.add_choice("", adder_default_choice);
+    adder.set_selected(0);
+
+	// adoption tree
+    if(!selector_below)
+	adopt(&adder);
+    adopt(&table);
+    if(selector_below)
+	adopt(&adder);
+
+	// events
+    adder.record_actor_on_event(this, new_line_to_add);
+
+	// visibility
+
+	// css stuff
+}
+
+
+void html_form_dynamic_table::add_obj_type(const string & label,
+					   const body_builder & for_brother)
+{
+    std::unique_ptr<body_builder> newone(for_brother.make_brother());
+
+    if(!newone)
+	throw WEBDAR_BUG;
+
+    list_of_types.push_back(std::move(newone));
+    adder.add_choice("", label);
+}
+
+void html_form_dynamic_table::on_event(const std::string & event_name)
+{
+    if(event_name == new_line_to_add)
+    {
+	if(adder.get_selected_num() != 0)
+	{
+		// there is a shift by one between adder which first position (index 0)
+		// tells the user to select a filter and list_of_mask_types that have the
+		// first valid mask at position 0.
+	    add_line(adder.get_selected_num() - 1);
+	    adder.set_selected(0); // resetting 'adder' to undefined
+	}
+    }
+    else // check whether this is an event from a delete button
+	del_line(event_name); // may throw exception if event_name is not a del event
+}
+
+
+string html_form_dynamic_table::inherited_get_body_part(const chemin & path,
+							const request & req)
+{
+    string ret = get_body_part_from_all_children(path, req);
+    purge_to_delete();
+    return ret;
+}
+
+
+void html_form_dynamic_table::add_line(unsigned int typenum)
+{
+    line newline;
+
+    if(typenum >= list_of_types.size())
+	throw WEBDAR_BUG;
+
+    if(! list_of_types[typenum])
+	throw WEBDAR_BUG;
+
+    if(left_label)
+	newline.left_label.reset(new (nothrow) html_text());
+    else
+	newline.left_label.reset();
+
+    newline.dynobj = list_of_types[typenum]->make_brother();
+    if(! newline.dynobj)
+	throw WEBDAR_BUG;
+
+    newline.del.reset(new (nothrow) html_form_input("delete",
+						    html_form_input::check,
+						    "",
+						    1));
+    if(! newline.del)
+	throw exception_memory();
+
+    string event_name = webdar_tools_convert_to_string(event_del_count++);
+    newline.del->set_change_event_name(event_name);
+    if(left_label)
+	table.adopt(&(*newline.left_label));
+    table.adopt(&(*newline.dynobj));
+    table.adopt(&(*newline.del));
+    list<line>::iterator pos = table_content.insert(table_content.end(),
+						    std::move(newline));
+    del_event_to_content[event_name] = pos;
+}
+
+void html_form_dynamic_table::del_line(const string & event_name)
+{
+    map<string, list<line>::iterator>::iterator mit = del_event_to_content.find(event_name);
+
+    if(mit == del_event_to_content.end())
+	throw WEBDAR_BUG; // event_name absent from the map!
+
+    list<line>::iterator it = mit->second;
+
+    if(left_label && !it->left_label)
+	throw WEBDAR_BUG;
+    if(!it->dynobj)
+	throw WEBDAR_BUG;
+    if(!it->del)
+	throw WEBDAR_BUG;
+
+    if(!it->left_label)
+	table.foresake(&(*(it->left_label)));
+    table.foresake(&(*(it->dynobj)));
+    table.foresake(&(*(it->del)));
+    events_to_delete.push_back(event_name);
+    	// postponing the object deletion
+	// as we may get from here from the it->del pointed to actor event
+	// we would not be able to return from this call if deleted from here
+}
+
+void html_form_dynamic_table::purge_to_delete()
+{
+    deque<string>::iterator evit = events_to_delete.begin();
+
+    while(evit != events_to_delete.end())
+    {
+	map<string, list<line>::iterator>::iterator mit = del_event_to_content.find(*evit);
+
+	if(mit == del_event_to_content.end())
+	    throw WEBDAR_BUG;  // event_name absent from the map!
+
+	list<line>::iterator it = mit->second;
+
+	(void)del_event_to_content.erase(mit);
+	(void)table_content.erase(it);
+
+	++evit;
+    }
+
+    events_to_delete.clear();
+}
+
---
+++ src/html_form_dynamic_table.hpp
@@ -0,0 +1,177 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_FORM_DYNAMIC_TABLE_HPP
+#define HTML_FORM_DYNAMIC_TABLE_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include <memory>
+#include <string>
+
+    // webdar headers
+#include "body_builder.hpp"
+#include "html_table.hpp"
+#include "html_button.hpp"
+#include "html_text.hpp"
+#include "html_form_input.hpp"
+#include "html_form_select.hpp"
+
+    /// class html_form_dynamic_table web user dynamically add new lines in a table
+
+    /// The lines of the table are defined by the developper user of this class
+    /// providing a set of body_builder objets which class has the make_brother()
+    /// method implemented to create a brandh-new object of the same inherited
+    /// class as the ones given as reference to define the table's line content
+    ///
+    /// this html components is consitued of:
+    ///
+    /// +----html_table-----------------------------+-------+
+    /// |       |                                   |       |
+    /// |+-----+|+-------------------------------+  |+-+    |
+    /// ||label|||body_builder objet             |  || | del|
+    /// |+-----+|+--------------------------------  |+-+    |
+    /// |+-----+|+-------------------------------+  |+-+    |
+    /// ||label|||body_builder objet             |  || | del|
+    /// |+-----+|+--------------------------------  |+-+    |
+    /// |+-----+|+-------------------------------+  |+-+    |
+    /// ||label|||body_builder objet             |  || | del|
+    /// |+-----+|+--------------------------------  |+-+    |
+    /// |+-----+|+-------------------------------+  |+-+    |
+    /// ||label|||body_builder objet             |  || | del|
+    /// |+-----+|+--------------------------------  |+-+    |
+    /// |+-----+|+-------------------------------+  |+-+    |
+    /// ||label|||body_builder objet             |  || | del|
+    /// |+-----+|+--------------------------------  |+-+    |
+    /// +-------+-----------------------------------+-------+
+    ///
+    /// +---------------------+
+    /// |  add drop down list | (or html_button if only one type to add
+    /// +---------------------+ (option to position the button before the table or after)
+    ///
+    ///
+    /// \note this component must be adopted by an html_form
+    /// \note the programmer should provide the components type
+    /// that will consititue each new line calling add_type()
+    /// and the provided body_builder objects must have their
+    /// method make_brother() re-defined to provide another object
+    /// (possibily a brand-new one or a copy constructed one if necessary)
+    /// of the exact same class (not of a parent class).
+
+class html_form_dynamic_table : public body_builder, public actor
+{
+private:
+    struct line; // referred before the public part for the ::iterator nested class
+
+public:
+	/// constructor
+    html_form_dynamic_table(bool has_left_labels,
+			    bool selector_below,
+			    const std::string & adder_text,
+			    const std::string & adder_default_choice);
+
+    html_form_dynamic_table(const html_form_dynamic_table & ref) = delete;
+    html_form_dynamic_table(html_form_dynamic_table && ref) noexcept = delete;
+    html_form_dynamic_table & operator = (const html_form_dynamic_table & ref) = delete;
+    html_form_dynamic_table & operator = (html_form_dynamic_table && ref) noexcept = delete;
+    ~html_form_dynamic_table() = default;
+
+    class iterator
+    {
+    public:
+	iterator() {};
+	iterator(const iterator & ref) = default;
+	iterator(iterator && ref) = default;
+	iterator & operator = (const iterator & ref) = default;
+	iterator & operator = (iterator && ref) = default;
+	~iterator() = default;
+
+	iterator & operator ++ (signed int n) { ++ptr; return *this; };
+	iterator operator ++ () { iterator ret; ret.ptr = ptr; ++ptr; return ret; };
+	std::shared_ptr<html_text> get_left_label() { return ptr->left_label; };
+	std::shared_ptr<body_builder> get_object() { return ptr->dynobj; };
+
+    private:
+	std::list<line>::iterator ptr;
+
+	friend html_form_dynamic_table;
+    };
+
+    iterator begin() { iterator ret; ret.ptr = table_content.begin(); return ret; };
+    iterator end() { iterator ret; ret.ptr = table_content.end(); return ret; };
+
+	/// add a new object type to be proposed to the user from the "adder" selector
+
+	/// \param[in] column the column number for this object (starts at zero)
+	/// \param[in,out] obj pointer to object that will be passed to the html_form_dynamic_table
+    void add_obj_type(const std::string & label, const body_builder & obj);
+
+    	/// clear table content
+    void clear() { table_content.clear(); del_event_to_content.clear(); events_to_delete.clear(); };
+
+	/// inherited from actor
+    virtual void on_event(const std::string & event_name) override;
+
+protected:
+	/// inherited methods from body_builder
+    virtual std::string inherited_get_body_part(const chemin & path,
+						const request & req) override;
+
+private:
+
+	// events
+    static constexpr const char* new_line_to_add = "new_line";
+
+	/// objects adopted for a given line of the "table" and managed by table_content
+    struct line
+    {
+	line() { left_label.reset(); dynobj.reset(); del.reset(); };
+
+	std::shared_ptr<html_text> left_label;
+	std::shared_ptr<body_builder> dynobj;
+	std::unique_ptr<html_form_input> del;
+    };
+
+    std::deque<std::unique_ptr<body_builder> > list_of_types; ///< available types of objects
+    std::list<line> table_content;       ///< manages the objects adopted by "table"
+    unsigned int event_del_count;        ///< used to build new event name for each new "delete" button
+    std::map<std::string, std::list<line>::iterator> del_event_to_content; /// find the table_content entry corresponding to a given event
+    std::deque<std::string> events_to_delete; ///< temporary list of events pending for deletion
+
+    bool left_label;
+    html_table table;
+    html_form_select adder;
+
+    void add_line(unsigned int typenum);
+    void del_line(const std::string & event_name);
+    void purge_to_delete();
+
+    friend html_form_dynamic_table::iterator;
+};
+
+#endif
