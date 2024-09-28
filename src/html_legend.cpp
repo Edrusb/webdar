b6c14d891932c278967e08ffc9f3129d74f72d81
Denis corbin <dar.linux@free.fr>
2024-09-28 13:38:32 +0200

    adding class html_legend

---
+++ src/html_legend.cpp
@@ -0,0 +1,60 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+
+
+
+    //
+#include "html_legend.hpp"
+
+using namespace std;
+
+void html_legend::change_label(const string & label)
+{
+    if(x_label != label)
+    {
+	x_label = label;
+	my_body_part_has_changed();
+    }
+}
+
+string html_legend::inherited_get_body_part(const chemin & path,
+						   const request & req)
+{
+    string ret = "";
+
+    ret += "<legend " + get_css_classes() + ">" + x_label + "</legend>\n";
+    ret += get_body_part_from_all_children(path, req);
+
+    return ret;
+}
---
+++ src/html_legend.hpp
@@ -0,0 +1,66 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef HTML_LEGEND_HPP
+#define HTML_LEGEND_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "body_builder.hpp"
+
+    /// class html_legend implements HTML legend feature
+
+    /// used within html_form_fieldset, mainly
+
+class html_legend : public body_builder
+{
+public:
+    html_legend(const std::string & label) { x_label = label; };
+    html_legend(const html_legend & ref) = default;
+    html_legend(html_legend && ref) noexcept = default;
+    html_legend & operator = (const html_legend & ref) = default;
+    html_legend & operator = (html_legend && ref) noexcept = default;
+    ~html_legend() = default;
+
+    void change_label(const std::string & label);
+    const std::string get_label() const { return x_label; };
+
+protected:
+	/// inherited from body_builder
+    virtual std::string inherited_get_body_part(const chemin & path,
+						const request & req) override;
+
+private:
+    std::string x_label;
+
+};
+
+#endif
