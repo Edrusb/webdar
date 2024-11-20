131767aafdc0b1aa2d3356858b17f9870ac32677
Denis corbin <dar.linux@free.fr>
2024-11-20 17:28:48 +0100

    adding disconnect_page class

---
+++ src/disconnected_page.cpp
@@ -0,0 +1,91 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+#include "my_config.h"
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+
+
+    //
+#include "disconnected_page.hpp"
+
+using namespace std;
+
+disconnected_page::disconnected_page():
+    page("disconnected from Webdar"),
+    redir(false)
+{
+
+	// component setup
+    msg.add_text(3, "You are now disconnected from Webdar");
+
+	// adoption tree
+    page.adopt(&msg);
+
+	// events
+
+
+	// css
+    static const char* text_css = "disconnected_page_text";
+    css_class text_class(text_css);
+    css tmp;
+
+    tmp.css_text_h_align(css::al_center);
+    tmp.css_text_v_align(css::al_middle);
+    text_class.set_value(tmp);
+    msg.add_css_class(text_css);
+
+    page.define_css_class_in_library(text_class);
+}
+
+answer disconnected_page::give_answer(const request & req)
+{
+    answer ret;
+
+    if(redir)
+    {
+	uri target(req.get_uri());
+	target.trim_path(); // to avoid pointing on the disconnect and force the user to select a session
+	page.set_refresh_redirection(0, target.get_string());
+    }
+    else
+	page.set_refresh_redirection(0, "");
+    ret.set_status(STATUS_CODE_OK);
+    ret.set_reason("ok");
+    ret.add_body(page.get_body_part(req.get_uri().get_path(), req));
+
+    return ret;
+}
+
+void disconnected_page::prefix_has_changed()
+{
+    page.set_prefix(get_prefix());
+}
---
+++ src/disconnected_page.hpp
@@ -0,0 +1,73 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef DISCONNECTED_PAGE_HPP
+#define DISCONNECTED_PAGE_HPP
+
+    // C system header files
+#include "my_config.h"
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include <dar/libdar.hpp>
+
+    // webdar headers
+#include "responder.hpp"
+#include "html_page.hpp"
+#include "html_text.hpp"
+
+    /// html component used for the user to define a has algorithm
+
+class disconnected_page : public responder
+{
+public:
+    static const std::string changed;
+
+    disconnected_page();
+    disconnected_page(const disconnected_page & ref) = default;
+    disconnected_page(disconnected_page && ref) noexcept = default;
+    disconnected_page & operator = (const disconnected_page & ref) = default;
+    disconnected_page & operator = (disconnected_page && ref) noexcept = default;
+    ~disconnected_page() = default;
+
+	/// wether to redirect to the uri with trimmed path
+    void set_redirect(bool mode) { redir = mode; };
+
+    	/// inherited from responder
+    virtual answer give_answer(const request & req) override;
+
+protected:
+    	/// inherited from responder
+    virtual void prefix_has_changed() override;
+
+private:
+    html_page page;
+    html_text msg;
+
+    bool redir;
+};
+
+#endif
