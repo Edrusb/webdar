ab493a8e8db8be89c674f7e70024efdab4af9811
Denis corbin <dar.linux@free.fr>
2025-01-03 16:50:29 +0100

    adding class guichet

---
+++ src/guichet.cpp
@@ -0,0 +1,370 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+#include <dar/tools.hpp>
+
+    // webdar headers
+
+
+
+    //
+#include "guichet.hpp"
+
+using namespace std;
+
+guichet::guichet(const shared_ptr<bibliotheque> & ptr,
+		 bibliotheque::category cat,
+		 const shared_ptr<body_builder> & to_adopt,
+		 bool add_form_around):
+    categ(cat),
+    biblio(ptr),
+    adopted(to_adopt),
+    ignore_events(false),
+    select_form("Update"),
+    select_fs(""),
+    select("Configuration", event_select),
+    around_adopted("Update"),
+    around_adopted_fs(""),
+    edit("Edit", event_edit),
+    clear("Clear", event_clear),
+    saveas_form("Save As"),
+    saveas_fs(""),
+    saveas_name("New configuration name", html_form_input::text, "", "50")
+{
+    if(!adopted)
+	throw WEBDAR_BUG;
+
+    adopted_jsoner = dynamic_cast<jsoner*>(adopted.get());
+    if(adopted_jsoner == nullptr)
+	throw WEBDAR_BUG;
+
+    adopted_subconfig = dynamic_cast<bibliotheque_subconfig*>(adopted.get());
+	// adopted_subconfig may be null if the adopted object
+	// does not implement this optional interface
+
+
+	// component configuration
+    update_selected();
+    set_adopted();
+
+    saveas_name.set_change_event_name(event_saveas);
+
+	// adption tree
+    select_fs.adopt(&select);
+    select_form.adopt(&select_fs);
+    adopt(&select);
+
+    if(add_form_around)
+    {
+	around_adopted_fs.adopt(adopted.get());
+	around_adopted.adopt(&around_adopted_fs);
+	adopt(&around_adopted);
+	adopted_frame = &around_adopted;
+    }
+    else
+    {
+	adopt(adopted.get());
+	adopted_frame = adopted.get();
+    }
+
+    if(adopted_frame == nullptr)
+	throw WEBDAR_BUG;
+
+    adopt(&edit);
+    adopt(&clear);
+
+    saveas_fs.adopt(&saveas_name);
+    saveas_form.adopt(&saveas_fs);
+    adopt(&saveas_form);
+
+	// events
+    select_form.record_actor_on_event(this, event_select);
+    edit.record_actor_on_event(this, event_edit);
+    clear.record_actor_on_event(this, event_clear);
+    saveas_name.record_actor_on_event(this, event_saveas);
+    biblio->record_actor_on_event(this, bibliotheque::changed);
+	// visibility
+    set_visibility();
+
+	// csss
+}
+
+void guichet::load_json(const json & source)
+{
+    json conf;
+    unsigned int vers;
+    string id;
+    bool mode;
+
+    try
+    {
+	conf = unwrap_config_from_json_header(source,
+					      vers,
+					      id);
+
+	if(id != string(class_id))
+	    throw exception_range(libdar::tools_printf("Unexpected class_id in json data, found %s while expecting %s",
+						       id.c_str(),
+						       class_id));
+
+	if(vers > jversion)
+	    throw exception_range("Json format version too hight for class guichet, upgrade your webdar software");
+
+	mode = conf.at(jlabel_mode);
+	update_selected(); // be sure to have all config name recorded
+	if(mode)
+	{
+		// manual mode
+
+	    select.set_selected(0);
+	    adopted_jsoner->load_json(conf.at(jlabel_conf));
+	}
+	else
+	{
+		// config mode
+
+	    string conf_cat = conf.at(jlabel_categ);
+	    string conf_name = conf.at(jlabel_name);
+	    bibliotheque::category newcateg = bibliotheque::string_to_category(conf_cat);
+
+	    if(newcateg != categ)
+		throw exception_range(libdar::tools_printf("Unexpected configuration category in json data: found %s, expected %s",
+							   conf_cat.c_str(),
+							   bibliotheque::category_to_string(categ)));
+	    try
+	    {
+		select.set_selected(conf_name);
+	    }
+	    catch(exception_base & e)
+	    {
+		    // any exception, include exception_bug means the config name does not exist
+
+		throw exception_range(libdar::tools_printf("Unavailable configuration name %s in category %s, the json configuration is not coherent",
+							  conf_name.c_str(),
+							  conf_cat.c_str()));
+	    }
+
+	    update_selected(); // this will load the configuration from the bibliotheque
+	}
+    }
+    catch(exception_bug & e)
+    {
+	throw;
+    }
+    catch(exception_base & e)
+    {
+	e.change_message(string("Error met while parsing json configuration for guichet object: ") + e.get_message());
+	throw;
+    }
+
+    set_visibility();
+}
+
+json guichet::save_json() const
+{
+    json ret;
+
+    if(select.get_selected_num() == 0)
+    {
+	    // manual mode
+
+	ret[jlabel_mode] = true;
+	ret[jlabel_conf] = adopted_jsoner->save_json();
+    }
+    else
+    {
+	    // config mode
+
+	ret[jlabel_mode] = false;
+	ret[jlabel_categ] = bibliotheque::category_to_string(categ);
+	ret[jlabel_name] = select.get_selected_id();
+    }
+
+
+    return wrap_config_with_json_header(jversion,
+					class_id,
+					ret);
+}
+
+void guichet::clear_json()
+{
+    adopted_jsoner->clear_json();
+}
+
+
+bibliotheque::using_set guichet::get_using_set() const
+{
+    bibliotheque::using_set ret;
+
+    if(select.get_selected_num() != 0)
+    {
+	bibliotheque::coordinates coord(categ, select.get_selected_id());
+	ret.insert(coord);
+    }
+
+    if(adopted_subconfig != nullptr)
+	subconfig_add_to(ret, adopted_subconfig->get_using_set());
+
+    return ret;
+}
+
+void guichet::on_event(const std::string & event_name)
+{
+    if(ignore_events)
+	return;
+
+    if(event_name == event_select)
+    {
+	set_visibility();
+    }
+    else if(event_name == event_edit)
+    {
+	if(select.get_selected_num() != 0)
+	{
+	    set_adopted();
+	    select.set_selected(0);
+		// this trigers an new event
+		// which sets visibility()
+	}
+	else
+	    throw WEBDAR_BUG;
+    }
+    else if(event_name == event_clear)
+    {
+	if(select.get_selected_num() != 0)
+	{
+	    adopted_jsoner->clear_json();
+	    select.set_selected(0);
+		// this trigers an new event
+		// which sets visibility()
+	}
+	else
+	    throw WEBDAR_BUG;
+    }
+    else if(event_name == event_saveas)
+    {
+	if(!biblio)
+	    throw WEBDAR_BUG;
+
+	if(saveas_name.get_value().empty())
+	    throw exception_range("Cannot save component with an empty name");
+
+	if(adopted_subconfig != nullptr)
+	{
+	    bibliotheque::using_set sub = adopted_subconfig->get_using_set();
+
+	    biblio->add_config(categ,
+			       saveas_name.get_value(),
+			       adopted_jsoner->save_json(),
+			       sub);
+	}
+	else
+	{
+	    biblio->add_config(categ,
+			       saveas_name.get_value(),
+			       adopted_jsoner->save_json());
+	}
+
+	    // config addition as here, does not trigger bibliotheque::changed
+	update_selected();
+
+	    // setting the selected to the new created configuration
+	try
+	{
+	    select.set_selected(saveas_name.get_value());
+	}
+	catch(exception_base & e)
+	{
+	    throw WEBDAR_BUG;
+	}
+    }
+    else if(event_name == bibliotheque::changed)
+    {
+	update_selected();
+    }
+    else
+	throw WEBDAR_BUG;
+}
+
+void guichet::update_selected()
+{
+    deque<string> available;
+
+    if(!biblio)
+	throw WEBDAR_BUG;
+    available = biblio->listing(categ);
+
+    ignore_events = true;
+    try
+    {
+	select.clear();
+	select.add_choice("0", "---- manual config ----");
+	for(deque<string>::iterator it = available.begin();
+	    it != available.end();
+	    ++it)
+	{
+	    select.add_choice(*it, *it); // id and label are the same
+	}
+    }
+    catch(...)
+    {
+	ignore_events = false;
+	throw;
+    }
+    ignore_events = false;
+
+    on_event(event_select); // calling the event only once
+}
+
+void guichet::set_adopted()
+{
+    if(select.get_selected_num() > 0) // not the "manual config"
+    {
+	if(!biblio)
+	    throw WEBDAR_BUG;
+
+	adopted_jsoner->load_json(biblio->fetch_config(categ, select.get_selected_id()));
+    }
+	// else adopted is unchanged
+}
+
+void guichet::set_visibility()
+{
+    bool manualmode = select.get_selected_num() == 0;
+
+    if(adopted_frame == nullptr)
+	throw WEBDAR_BUG;
+    adopted_frame->set_visible(manualmode);
+    edit.set_visible(!manualmode);
+    clear.set_visible(manualmode);
+    saveas_form.set_visible(manualmode);
+    if(!manualmode)
+	saveas_name.set_value("");
+}
---
+++ src/guichet.hpp
@@ -0,0 +1,168 @@
+/*********************************************************************/
+// webdar - a web server and interface program to libdar
+// Copyright (C) 2013-2024 Denis Corbin
+//
+// This file is part of Webdar
+//
+//  Webdar is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  Webdar is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with Webdar.  If not, see <http://www.gnu.org/licenses/>
+//
+//----
+//  to contact the author: dar.linux@free.fr
+/*********************************************************************/
+
+#ifndef GUICHET_HPP
+#define GUICHET_HPP
+
+    // C system header files
+extern "C"
+{
+
+}
+
+    // C++ system header files
+
+
+    // webdar headers
+#include "body_builder.hpp"
+#include "jsoner.hpp"
+#include "bibliotheque.hpp"
+#include "bibliotheque_subconfig.hpp"
+#include "actor.hpp"
+#include "html_form.hpp"
+#include "html_form_fieldset.hpp"
+#include "html_form_select.hpp"
+#include "html_double_button.hpp"
+#include "html_form_input.hpp"
+
+
+    /// class guichet let one fetch config from a bibliotheque, save as or setup manually for a given component type
+
+    /// the given component is adopted by the guichet and is shown only in manual mode, else it is hidden.
+    /// The guichet object adds a drop-down list of available configurations for that objects (found in the
+    /// provided bibliotheque), beside several buttons:
+    /// - edit : which lead the component to show and the dropbox to be set to manual mode
+    /// - save-as : in manual mode to save the current configuration as a new one in the bibliotheque
+    ///   (the save-as button is associated to a box to provide a name)
+    /** \verbatime
+
+	+--------<form>-----------------------------------+
+	| +----------------------------------+            |
+        | | <drop down> list (incl. "manual")|            |
+	| +----------------------------------+            |
+	|                                                 |
+	+---<update>--------------------------------------+
+
+	(to_adopt object show only in "manual" mode)
+	+-----------<optional form around to_adopt>-------+
+	| < to_adopt>                                     |
+	|                                                 |
+	+---<update>--------------------------------------+
+
+	(edit shows if non-"manual" choice is selected here above)
+	(clear shows only in "manual" mode)
+	+---------------+ +---------------+
+	| edit config   | | clear config  |
+	+---------------+ +---------------+
+
+	(the below form only shows if to_adopt is visible ("manual" mode))
+	+---<save-as-form>--------------------------------+
+	|                     +-------------------------+ |
+	| configuration name  |                         | |
+	|                     +-------------------------+ |
+	|                                                 |
+	+---<save as>-------------------------------------+
+
+	\endverbatim **/
+
+class guichet: public body_builder, public jsoner, public bibliotheque_subconfig, public actor
+{
+public:
+
+	/// constructor
+
+	/// \param[in] ptr the bibliotheque object where to fetch from configurations
+	/// \param[in] cat the category for configurations to use in the bibliotheque
+	/// \param[in] to_adopt the jsoner inherited object to adopt and apply configurations to.
+	/// Important: the pointed to object by to_adopt is not expected to be changed during the
+	/// lifetime of this guichet object!
+	/// \param[in] add_form_around adopt to_adopt in a form in turn adopted by "this" (needed for
+	/// to update components manually when they do not include a form themselves).
+	/// \note the to_adopt pointed to object is interrogated directly by owner of guichet to fetch the
+	/// specific object properties, guichet only uses its jsoner interface, it is also expected to
+	/// be a body_builder inherited object for it can be adopted by "this", and of course it must
+	/// not have already been adopted. This object will not be visible when using an existing
+	/// configuration in the bibliotheque, or visible when setup manually.
+	/// \note the object that owns the guichet should not fetch json stuff from the to_adopt object
+	/// but should pass through this guichet object to either get the to_adopted json config (wrapped
+	/// with some metadata provided by guichet) or a guichet structure pointing to the configuration
+	/// name to use in the bibliotheque.
+    guichet(const std::shared_ptr<bibliotheque> & ptr,
+	    bibliotheque::category cat,
+	    const std::shared_ptr<body_builder> & to_adopt,
+	    bool add_form_around);
+
+	/// inherited from jsoner (used when this object is used as subconfig of another object)
+    virtual void load_json(const json & source) override;
+
+	/// inherited from jsoner (used when this object is used as subconfig of another object)
+    virtual json save_json() const override;
+
+	/// inherited from jsoner (used when this object is used as subconfig of another object)
+    virtual void clear_json() override;
+
+	/// inherited from bibliotheque_subconfig
+    virtual bibliotheque::using_set get_using_set() const override;
+
+	/// inherited from actor
+    virtual void on_event(const std::string & event_name) override;
+
+private:
+    bibliotheque::category categ;
+    std::shared_ptr<bibliotheque> biblio;
+    std::shared_ptr<body_builder> adopted;
+    jsoner* adopted_jsoner;
+    bibliotheque_subconfig* adopted_subconfig; ///< may be null if adopted does not implement bibliotheque_subconfig interface
+    bool ignore_events;
+    body_builder* adopted_frame;   ///< which wrapper object to change visibility of
+
+    html_form select_form;
+    html_form_fieldset select_fs;
+    html_form_select select;
+    html_form around_adopted;
+    html_form_fieldset around_adopted_fs;
+    html_double_button edit;
+    html_double_button clear;
+    html_form saveas_form;
+    html_form_fieldset saveas_fs;
+    html_form_input saveas_name;
+
+    void update_selected();
+    void set_adopted();
+    void set_visibility();
+
+    static constexpr const char* event_select = "select";
+    static constexpr const char* event_edit = "edit";
+    static constexpr const char* event_clear = "clear";
+    static constexpr const char* event_saveas = "saveas";
+
+    static constexpr const unsigned int jversion = 1;
+    static constexpr const char* class_id = "guichet";
+    static constexpr const char* jlabel_mode = "manual-mode"; // boolean (either categ+name or config)
+    static constexpr const char* jlabel_categ = "category";  // string: config category
+    static constexpr const char* jlabel_name = "configname"; // string: config name
+    static constexpr const char* jlabel_conf = "config";   ; // json; object configuration
+};
+
+
+#endif
