671be76531784fc0231ee86c62abbdcc82940823
Denis corbin <dar.linux@free.fr>
2025-01-27 14:33:53 +0100

    having html_compression_params propagating assigned CSS classes to its internal html_form object

--- src/html_compression_params.cpp
+++ src/html_compression_params.cpp
@@ -276,3 +276,14 @@ string html_compression_params::inherited_get_body_part(const chemin & path,
 {
     return get_body_part_from_all_children(path, req);
 }
+
+void html_compression_params::css_classes_have_changed()
+{
+    set<string> css_classes = get_css_classes_as_a_set();
+
+    form_compr.clear_css_classes();
+    for(set<string>::iterator it = css_classes.begin();
+	it != css_classes.end();
+	++it)
+	form_compr.add_css_class(*it);
+}
--- src/html_compression_params.hpp
+++ src/html_compression_params.hpp
@@ -100,6 +100,8 @@ protected:
     virtual std::string inherited_get_body_part(const chemin & path,
 						const request & req) override;
 
+	/// inherited from body_builder
+    virtual void css_classes_have_changed() override;
 
 private:
     html_form form_compr;
