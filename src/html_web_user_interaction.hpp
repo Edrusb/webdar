295e5003f9a2c2e1b9d03600ad47b3c49e15cfa8
Denis corbin <dar.linux@free.fr>
2025-02-14 10:23:24 +0100

    fixing syntax in documentation

--- src/html_web_user_interaction.hpp
+++ src/html_web_user_interaction.hpp
@@ -88,7 +88,7 @@ extern "C"
     /// The caller should use get_user_interaction() to provide a user interaction to a libthreadar::thread object
     /// for it can interact with the user thanks to this html_web_user_interaction but the thread should not be
     /// run by the caller (though the caller will still have to join() to cleanup/propagate its exception status),
-    /// but passed to the run_and_control_thread() method for that.
+    /// but passed to the run_and_control_thread() method instead.
     /// the caller (code) can still access the thread. While the user (Web UI/browser) can also stop the thread
     /// using the button that show in turn at the bottom of the component. Once the thread has finished, the component
     /// can automatically hide from the UI (auto_hide(true)) or stay visible until the user press the "finish"
