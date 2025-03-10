b1cda6571493ce71e56d1f35e7f5aae780ad7485
Denis corbin <dar.linux@free.fr>
2025-03-10 18:31:00 +0100

    removing a risk of page refreshing ending before the component get invisible

--- src/html_web_user_interaction.cpp
+++ src/html_web_user_interaction.cpp
@@ -469,14 +469,17 @@ void html_web_user_interaction::set_mode(mode_type m)
 	ask_close.set_visible(false);
 	force_close.set_visible(false);
 	finish.set_visible(true);
-	act(dont_refresh);
 	if(!autohide || (was_interrupted && hide_unless_interrupted))
+	{
+	    act(dont_refresh);
 	    break; // break for the 'case finished:' !!!
+	}
 	else // if auto hide is set, we go to the closed status, here below
 	    m = closed;
 	    // no break!
     case closed:
 	set_visible(false); // we hide ourself
+	act(dont_refresh);
 	act(libdar_has_finished);
 	break;
     default:
